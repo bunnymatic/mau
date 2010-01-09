7cf22a72ba4465caa1eb299c686e3bad4a049736
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2010-01-09 22:52:26 +0000

    Fixed up submission forms so that we actually get the data.
    Updated javascript interaction so send button is disabled until you do something on the form

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -26,8 +26,8 @@ class FeedbacksController < ApplicationController
       render 'thankyou', :status => :created
     else
       @error_message = "Please enter a comment or hit cancel."
-	  
-	  # Returns the whole form back. This is not the most effective
+      
+      # Returns the whole form back. This is not the most effective
       # use of AJAX as we could return the error message in JSON, but
       # it makes easier the customization of the form with error messages
       # without worrying about the javascript.
