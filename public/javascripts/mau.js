4f79da9cdd910c2761e080555c41593458a6b1fe
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2010-03-03 04:11:41 +0000

    fixes #327 - put up uploading wait box`

--- public/javascripts/mau.js
+++ public/javascripts/mau.js
@@ -44,7 +44,16 @@ MAU = window['MAU'] || {};
 
     // Changes the cursor to an hourglass
     M.waitcursor = function() {
-	document.body.style.cursor = 'wait';
+		var dv = document.createElement('div');
+		Element.extend(dv);
+		dv.addClassName('wait-dialog');
+		dv.show();
+		var tx = document.createTextNode('Uploading...');
+		dv.appendChild(tx);
+		var im = document.createElement('img');
+		im.setAttribute('src','/images/spinner32.gif');
+		dv.appendChild(im);
+		document.body.appendChild(dv);
     };
     
     // Returns the cursor to the default pointer
