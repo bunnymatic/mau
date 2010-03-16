9793acd7d37c1a6a460697f028cb9c7af81febf4
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2010-03-16 23:31:08 +0000

    Added Katja's feed to the feed list and did better "cleanup" of the feed contents

--- public/javascripts/mau_feeds.js
+++ public/javascripts/mau_feeds.js
@@ -4,16 +4,25 @@ MAUFEEDS = window['MAUFEEDS'] || {};
     /** all js init goes in here. **/
 
     MAUFEEDS.init = function() {
-	var url = document.location.href;
-	new Ajax.Request('/feeds/feed', {
-	    parameters: { authenticity_token:unescape(authenticityToken), numentries:1, page:url},
-	    onSuccess: function(transport) {
-		var d = $('feed_div');
-		d.innerHTML = transport.responseText;
-	    }
-	});
-	MAUFEEDS.init = function(){};
-    };
+		var url = document.location.href;
+		new Ajax.Request('/feeds/feed', {
+			parameters: { authenticity_token:unescape(authenticityToken), numentries:1, page:url},
+			onSuccess: function(transport) {
+				var d = $('feed_div');
+				d.innerHTML = transport.responseText;
+				MAU.log("Got feeds");
+				MAUFEEDS.stripStyles('feed_div');
+			}
+		});
+		MAUFEEDS.init = function(){};
+	};
+	MAUFEEDS.stripStyles = function( feeddiv_id ) {
+		MAU.log( "Strip");
+		spans = $$('#'+feeddiv_id+" span");
+		MAU.log( "spans " + spans.length);
+		// clean style attributes from spans
+		spans.each(function(sp){ sp.writeAttribute('style','');});
+	};
 
     Event.observe(window, 'load', MAUFEEDS.init);
 }
