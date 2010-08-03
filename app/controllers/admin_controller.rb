88afd3b303b451a4896c70bf3fc84869409ceb12
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2010-08-03 09:14:41 +0000

    * added artists_per_day admin page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,9 +3,22 @@ class AdminController < ApplicationController
   def index
     render :text => "Nothing to see here.  Please move along."
   end
-
+  
   def stats
     render :text => "<html><body>%s Artists<br/>%s ArtPieces<br/></body></html>" % [ Artist.count, ArtPiece.count ]
+  end
+
+  def artists_per_day
+    sql = ActiveRecord::Base.connection()
+    @tbl = []
+    cur = sql.execute "select count(*) ct,date(activated_at) d from artists group by d order by d desc;"
+    ctr = 0
+    cur.each_hash do |h|
+      @tbl << [h['ct'],h['d']]
+    end
+    @numrows = @tbl[-1][0].to_i
+    @tbl.delete_at(-1)
+    
   end
 
   def email_artists
