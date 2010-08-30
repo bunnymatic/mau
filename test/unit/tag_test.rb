5f1701d21429c5fab40df4fea007dfc76ecfe5e2
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2010-08-30 23:25:42 +0000

    * updated tag tests

--- test/unit/tag_test.rb
+++ test/unit/tag_test.rb
@@ -2,7 +2,32 @@ require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :tags
+  test "add delete tag" do
+    t = tags(:one)
+    _id = t.id
+    t.save()
+    tt = Tag.find(_id)
+    assert_equal(tt.name, t.name)
+    tt.delete()
+    caught = false
+    begin
+      ttt = Tag.find(_id)
+    rescue ActiveRecord::RecordNotFound
+      caught = true
+    end
+    assert(caught)
+  end
+
+  test "safe name" do
+    t = tags(:with_spaces)
+    assert( t.safe_name.include?('&nbsp;') )
+  end
+  # TODO: this should really fail - don't allow tags that are only whitespace
+  # requires model update
+  test "add empty tag" do
+    t = tags(:empty)
+    _id = t.id
+    t.save()
   end
 end
