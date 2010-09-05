f71f9e165a184f31c5eda0e5472b67a589822ce6
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2010-09-05 04:06:14 +0000

    * optimize images with jpegtran

---
+++ script/optimize_images.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os
+import os.path
+import sys
+
+global verbose
+verbose = 0
+    
+def main(srcdir, destdir):
+    # grab all jpegs in source tree and optimize them with jpegtrans
+    # in the destination directory
+    abs_src = os.path.abspath(srcdir)
+    abs_dest = os.path.abspath(destdir)
+    jpgs = []
+    for root, dirs, files in os.walk(srcdir):
+        for name in files:
+            if name.lower().endswith('jpg'):
+                jpgs.append(os.path.abspath(os.path.join(root, name)))
+    conversion = [ (jpg, jpg.replace(abs_src, abs_dest)) for jpg in jpgs ]
+    for c in conversion:
+        src, dest = c
+        destdir = os.path.dirname(dest)
+        if not os.path.exists(destdir):
+            os.makedirs(destdir)
+        os.system('jpegtran -optimize %s > %s' % (src,dest))
+
+        
+def usage():
+    print """Run jpegtran on all .jpg images in a subdirectory placing the
+    optimized version in the dest dir (-h for help)"""
+
+if __name__ == '__main__':
+    import optparse
+
+    p = optparse.OptionParser(usage=usage())
+    p.add_option('-v', '--verbose', action='store', dest='verbose',
+                 default=1, type="int",
+                 help='verbose level 0-9.  Default = 1')
+    p.add_option('-s', '--srcdir', action='store', dest='srcdir',
+                 help='Source directory')
+    p.add_option('-d', '--destdir', action="store", dest="destdir",
+                 help="Destination directory")
+    (options, rest) = p.parse_args()
+    verbose = options.verbose
+    if not options.destdir or not options.srcdir:
+        print "You must specify both a source and destination directory"
+    else:
+        main(options.srcdir, options.destdir)
+
+
