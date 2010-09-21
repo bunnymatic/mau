fe721587942a9196df31fb31abd2d9447cffa5ca
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2010-09-21 06:57:34 +0000

    * improved overlapping/duplicate/casetwin tags handling  * updated tag tests  * improved hiding of tag dropdown when other inputs are selected  * cleaned up failing studio test(cache test was obsolete)

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,18 +1,21 @@
 module TagsHelper
   def self.tags_to_s(tags)
-    (tags.collect {|t| t.name }).join(", ")
+    (tags.collect {|t| t.name }.uniq).join(", ")
   end
 
   def self.tags_from_s(tagstr)
     # split up tagstring into array of tags
-    tagnames = tagstr.strip.split(',').each { |h| h.strip! }
+    tagnames = tagstr.strip.split(',').each { |h| h.strip! }.uniq
     tags = []
     alltags = Tag.all
     tag_lut = {}
-    alltags.map { |t| tag_lut[t.name] = t }
+    alltags.map do |t| 
+      if t && t.name != nil && !t.name.empty?
+        tag_lut[t.name.downcase] = t 
+      end
+    end
     tagnames.each do |tg|
-      next if tg.empty?
-      tag = tag_lut[tg]
+      tag = tag_lut[tg.downcase]
       if !tag 
         tag = Tag.new
         tag.name = tg
