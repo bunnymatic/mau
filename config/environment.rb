3ee7f8818f42b95e7231ed211133048da4f028d4
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2010-11-27 22:41:49 +0000

    * added tests around presence of heart/note for loggedin logged out  * refs #277

--- config/environment.rb
+++ config/environment.rb
@@ -36,6 +36,7 @@ Rails::Initializer.run do |config|
   config.gem "json"
   config.gem "mysql"
   config.gem "i18n", :version => '0.3.7'
+  config.gem "fcgi"
   # config.gem "has_many_polymorphs"
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
