35c292aeb203c49c21af9b75f073b8e714344b62
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2010-12-24 23:19:24 +0000

    * more progress on notes refs #478

--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -1,1 +1,16 @@
 // Use this file to require common dependencies or to setup useful test functions.
+
+var get_events = function(sel, event_name) {
+  var events = [];
+  try {
+    var evs = $$(sel)[0].getStorage().get('prototype_event_registry');
+    events = evs.get(event_name);
+  } 
+  catch(e) {}
+  return events; 
+};
+
+var get_click_events = function(sel) {
+  return get_events(sel, 'click');
+};
+
