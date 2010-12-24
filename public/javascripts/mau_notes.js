a1ad583b5baee022425c97fe96a021bb91df788a
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2010-12-24 09:40:19 +0000

    * more notes progress  * very excited about blue ridge tests

--- public/javascripts/mau_notes.js
+++ public/javascripts/mau_notes.js
@@ -9,30 +9,46 @@ MAU = window['MAU'] || {};
 
 MAU.NotesMailer = Class.create();
 
+var constructEmailListMessage = function() {
+  var el = new Element('div');
+  el.innerHTML = "Awesome!  We'll notify you of upcoming MAU events.  We hate spam just like you do so the only things you'll be apprised of will be great!  Enter your email twice below and we'll put you on the list.";
+  $(el).insert({bottom:new Element('input', { type: 'text', id: 'email' })});
+  return el;
+}
+
 Object.extend(MAU.NotesMailer.prototype, {
+  selector: null,
+  note_class: null,
   notes: {
-    email_list: "<div>email list</div",
+    email_list: constructEmailListMessage(), 
     inquiry: "<div>inquiry</div>"
   },
-  insert: function(selector, key) {
+  insert: function() {
     var _that = this;
-    $$(selector).each(function(el) {
-      el.insert(_that[key]);
+    $$(this.selector).each(function(el) {
+      var clz = '.'+_that.note_class;
+      var subels = $(el).select(clz);
+      $(subels).each( function(subel) {
+        var s = $(subel).insert('<div class="popup-mailer"></div>');
+        $(s).insert(_that.notes[_that.note_class]);
+      });
     });
   },
   initialize: function(selector, note_class) {
+    this.selector = selector;
+    this.note_class = note_class;
     var _that = this;
     $$(selector).each(function(el) {
-      el.insert('<div class="' + note_class +'">');
-      $$(selector).each(function(root) {
-        $(root).select("." + note_class).each(function(notehead) {
-          if (note_class in _that.notes) {
+      if (note_class in _that.notes) {
+        el.insert('<div class="' + note_class +'">');
+        $$(selector).each(function(root) {
+          $(root).select("." + note_class).each(function(notehead) {
             $(notehead).observe('click', function (){ 
-              _that.insert( _that.notes[note_class]); 
+              _that.insert(); 
             });
-          }
+          });
         });
-      });
+      }
     });
   }
 });
