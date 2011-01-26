c6c4c397713952783e17cfc285a92e9e827d3c76
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2011-01-26 04:47:52 +0000

    * added mocha and rspec to init for non production

--- config/environment.rb
+++ config/environment.rb
@@ -39,7 +39,12 @@ Rails::Initializer.run do |config|
   config.gem "i18n", :version => '0.4.2'
   #config.gem "fcgi"
   config.gem "haml"
-  
+
+  if ENV['RAILS_ENV'] != 'production'
+    config.gem 'mocha'
+    config.gem 'rspec'
+    config.gem 'rspec-rails'
+  end
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
