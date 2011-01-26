208c42df5681cbd96758761bdcea8ce79dfcaef3
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2011-01-26 16:27:12 +0000

    *mocha dependencies added

--- config/environment.rb
+++ config/environment.rb
@@ -41,9 +41,7 @@ Rails::Initializer.run do |config|
   config.gem "haml"
 
   if ENV['RAILS_ENV'] != 'production'
-    config.gem 'mocha'
-    config.gem 'rspec'
-    config.gem 'rspec-rails'
+    config.gem 'mocha', :version -> '0.9.10'
   end
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
