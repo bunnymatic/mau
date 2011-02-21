040345b7a94e71fc5a5b769a4b492e5a73c3b6f9
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2011-02-21 07:28:23 +0000

    * new map computation

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -1,11 +1,12 @@
 module CatalogHelper
   def self.compute_position(map_size, lat, lng)
-    map_lats = [ 37.75032407630291, 37.76762766157575 ]
-    map_lngs = [ -122.426586614379, -122.404232889]
+    map_lats = [ 37.75008654795525, 37.767763359972506 ]
+    map_lngs = [ -122.42721, -122.40455181274414 ] 
+
     latrange = map_lats[1] - map_lats[0]
     lngrange = map_lngs[1] - map_lngs[0]
-    xrange = map_size[0]
-    yrange = map_size[1]
+    xrange = map_size[1]
+    yrange = map_size[0]
     
     [ xrange.to_f * ((lat - map_lats[0])/latrange) ,
       yrange.to_f * ((lng - map_lngs[0])/lngrange) ]
