81f1f09669b40aea818e9eb85a703b2f3176a6c4
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2011-02-22 02:10:09 +0000

    * updated catalog page to include "list of os artists"  * updated javascript includes

--- public/javascripts/mau_admin.js
+++ public/javascripts/mau_admin.js
@@ -2,47 +2,52 @@
 MAUAdmin =  window['MAUAdmin'] || {};
 
 (function() {
-    var M = MAUAdmin;
-    M.init = function() {
-	$('update_artists').observe('click', function() {
-	    var oss = $$('.os2010');
-	    var cbs = $$('.cb2010');
-	    var ii = 0;
-	    var updates = {};
-	    for ( ; ii < oss.length; ++ii ) {
-		os = oss[ii];
-		cb = cbs[ii];
-		
-		if ((os.innerHTML === 'true') !== (cbs[ii].checked)) {
-		    updates["ARTIST"+os.readAttribute('artistid')] = (cbs[ii].checked).toString();
-		}
-	    }
-	    var oss = $$('.spring2011');
-	    var cbs = $$('.cbspring2011');
-	    var ii = 0;
-	    var updates = {};
-	    for ( ; ii < oss.length; ++ii ) {
-		os = oss[ii];
-		cb = cbs[ii];
-		
-		if ((os.innerHTML === 'true') !== (cbs[ii].checked)) {
-		    updates["ARTIST"+os.readAttribute('artistid')] = (cbs[ii].checked).toString();
-		}
-	    }
+  var M = MAUAdmin;
+  M.init = function() {
+    $$('button.update-artists').each(function(el) {
+      el.observe('click', function() {
+	var oss = $$('.os2010');
+	var cbs = $$('.cb2010');
+	var ii = 0;
+	var updates = {};
+        if (oss && cbs) {
+	  for ( ; ii < oss.length; ++ii ) {
+	    os = oss[ii];
+	    cb = cbs[ii];
 	    
-	    var form = new Element('form', {
-                action: "/admin/artists/update",
-                method: "post"
-            });
-	    form.appendChild(new Element('input', { type:"hidden", name:"authenticity_token", value:unescape(authenticityToken)}));
-	    for (k in updates) {
-		var val = updates[k];
-		form.appendChild(new Element('input', { type:"hidden", name:k, value:val }));
+	    if ((os.innerHTML === 'true') !== (cbs[ii].checked)) {
+	      updates["ARTIST"+os.readAttribute('artistid')] = (cbs[ii].checked).toString();
 	    }
-	    document.body.appendChild(form);
-	    form.submit();
-	    return false;
-	});
-    };
-    Event.observe(window,'load',M.init);
+	  }
+        }
+	var oss = $$('.spring2011');
+	var cbs = $$('.cbspring2011');
+	var ii = 0;
+	var updates = {};
+        if (oss && cbs) {
+	  for ( ; ii < oss.length; ++ii ) {
+	    os = oss[ii];
+	    cb = cbs[ii];
+	    
+	    if ((os.innerHTML === 'true') !== (cbs[ii].checked)) {
+	      updates["ARTIST"+os.readAttribute('artistid')] = (cbs[ii].checked).toString();
+	    }
+	  }
+	}
+	var form = new Element('form', {
+          action: "/admin/artists/update",
+          method: "post"
+        });
+	form.appendChild(new Element('input', { type:"hidden", name:"authenticity_token", value:unescape(authenticityToken)}));
+	for (k in updates) {
+	  var val = updates[k];
+	  form.appendChild(new Element('input', { type:"hidden", name:k, value:val }));
+	}
+	document.body.appendChild(form);
+	form.submit();
+	return false;
+      });
+    });
+  };
+  Event.observe(window,'load',M.init);
 })();
