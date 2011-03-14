4008a32b579adb8013c11bdc9a33db3acca33d8e
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2011-03-14 02:38:04 +0000

    * more mobile styling to add stuff for open studios participation

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -3,9 +3,13 @@ module MobileHelper
   def render_as_mobile_list_item (content, opts)
     link = opts[:link] || '#'
     clz = opts[:xtra_class] || ''
-    
+    os_star = opts[:os_star]
+    if os_star
+      clz += " os-star"
+      star_code = "<div class='os-star'></div>"
+    end
     return <<EOM
-       <li class="mobile-menu #{clz}"><a data-transition="slide" rel="external" href="#{link}">#{content}</a></li>
+       <li class="mobile-menu #{clz}"><a data-transition="slide" rel="external" href="#{link}">#{star_code}#{content}</a></li>
 EOM
   end
 
