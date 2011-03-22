b2ab434fd048d73af717bd0cb283040db969ce60
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2011-03-22 10:53:53 +0000

    * added script to find artists by name

---
+++ script/find_artists_by_name.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+
+puts "Searching..."
+artists = []
+$stdin.read.split("\n").each do |line|
+  next if line.blank?
+  next if /^#/.match(line)
+  name_bits = line.split
+  next if name_bits.length < 2
+  a = Artist.find_by_firstname_and_lastname(name_bits[0].downcase, name_bits[1].downcase)
+  if a
+    artists << a
+  end
+end
+puts "Found #{artists.count} artists that match"
+artists.map{|a| "#{a.get_name}\t#{a.get_share_link}"}.each do |astr|
+  puts astr
+end
+
