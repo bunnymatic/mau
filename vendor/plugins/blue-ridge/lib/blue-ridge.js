f6c3f0e519232c4e28119e7838f46e03bc02a4af
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2011-07-10 21:39:34 +0000

    * trying all kinds of things to get js commandline spec to work

--- vendor/plugins/blue-ridge/lib/blue-ridge.js
+++ vendor/plugins/blue-ridge/lib/blue-ridge.js
@@ -2,12 +2,13 @@ var BlueRidge = BlueRidge || {};
 
 BlueRidge.Browser = {
   require: function(url, options){
+    
     options = options || {};
-
     var system   = options["system"] || false;
     var callback = options["onload"] || null;
 
     url = (system) ? this.treatUrlAsRelativeToCurrentFile(url) : this.treatUrlAsRelativeToSpecFile(url);
+
     this.createScriptTag(url, callback);
   },
 
@@ -19,7 +20,6 @@ BlueRidge.Browser = {
     var prefix = "^";
     for(var i=0; i < (depth-1); i++) { prefix += "\.\.\/" }
     url = url.replace(new RegExp(prefix), ""); 
-    
     return this.urlCorrection(depth) + url;
   },
   
@@ -30,6 +30,8 @@ BlueRidge.Browser = {
   createScriptTag: function(url, onload){
     var head = document.getElementsByTagName("head")[0];
     var script = document.createElement("script");
+    // force </script> tag (don't self close)
+    script.innerHTML = ' ';
 
     script.src = url;
     if(onload) { script.onload = script.onreadystatechange = onload; }
@@ -64,22 +66,26 @@ BlueRidge.Browser = {
 if(BlueRidge.loaded != true) {
   var require = function(url, options){ return BlueRidge.Browser.require(url, options) };
   var debug   = function(message)     { return BlueRidge.Browser.debug(message) };
-
   var BLUE_RIDGE_PREFIX = BLUE_RIDGE_PREFIX || "../../vendor/plugins/blue-ridge/";
   var BLUE_RIDGE_VENDOR_PREFIX = BLUE_RIDGE_PREFIX + "/vendor/";
 
-  require(BLUE_RIDGE_VENDOR_PREFIX + "jquery-1.4.2.js",    {system: true});
-  require(BLUE_RIDGE_VENDOR_PREFIX + "jquery.fn.js",       {system: true});
-  require(BLUE_RIDGE_VENDOR_PREFIX + "jquery.print.js",    {system: true});
-  require(BLUE_RIDGE_VENDOR_PREFIX + "screw.builder.js",   {system: true});
-  require(BLUE_RIDGE_VENDOR_PREFIX + "screw.matchers.js",  {system: true});
-  require(BLUE_RIDGE_VENDOR_PREFIX + "screw.events.js",    {system: true});
-  require(BLUE_RIDGE_VENDOR_PREFIX + "screw.behaviors.js", {system: true});
-  require(BLUE_RIDGE_VENDOR_PREFIX + "smoke.core.js",      {system: true});
-  require(BLUE_RIDGE_VENDOR_PREFIX + "smoke.mock.js",      {system: true});
-  require(BLUE_RIDGE_VENDOR_PREFIX + "smoke.stub.js",      {system: true});
-  require(BLUE_RIDGE_VENDOR_PREFIX + "screw.mocking.js",   {system: true});
+  var files = [
+    "jquery-1.4.2.js",   
+    "jquery.fn.js",      
+    "jquery.print.js",   
+    "screw.builder.js",  
+    "screw.matchers.js", 
+    "screw.events.js",   
+    "screw.behaviors.js",
+    "smoke.core.js",     
+    "smoke.mock.js",     
+    "smoke.stub.js",     
+    "screw.mocking.js"   ];
+  for (var idx in files) {
+    require(BLUE_RIDGE_VENDOR_PREFIX + files[idx], {system:true});
+  }
 
   BlueRidge.loaded = true;
   require(BlueRidge.Browser.deriveSpecNameFromCurrentFile());
+
 }
