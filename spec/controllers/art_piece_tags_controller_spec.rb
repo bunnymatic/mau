091e605e6863a95808dca7581d9b0b8697aa96ae
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2011-08-04 06:41:33 +0000

    * added missing test files

---
+++ spec/controllers/art_piece_tags_controller_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../controllers_helper')
+
+include AuthenticatedTestHelper
+
+describe ArtPieceTagsController do
+end
