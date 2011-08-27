582f475f94971b014bf44b644acc1707a0d47e38
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2011-08-27 23:33:16 +0000

    * gem updates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
+ENV["RAILS_ENV"] ||= 'test'
 require 'thread'
 require 'rubygems'
 # -*- coding: undecided -*-
 # This file is copied to ~/spec when you run 'ruby script/generate rspec'
 # from the project root directory.
-ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
 require 'spec/autorun'
 require 'spec/rails'
