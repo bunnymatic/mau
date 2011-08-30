2e2805f6f3a14344a8d0d47e4192d0ddf3717ad5
jon <jon@0663adc4-ecd5-4071-a5f5-e50769700564>
2011-08-30 08:06:25 +0000

    * moved twitter tests

---
+++ spec/lib/mau_tweeter_spec.rb
@@ -0,0 +1,23 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe MauTweeter do
+  describe 'tweet' do
+    it 'authenticates our twitter user' do
+      auth = { :type => :oauth,
+        :consumer_key => 'V7Nif8IiVQnajQ3XUslwcg',
+        :consumer_secret => 'UUvDXjUu6SK8DxrqwNNSYrcROYP0osMJmPUSOpxZ0',
+        :token => '62131363-mPnEym1H3rBeA0wKnqNRAPMpSBr6UZOdLpUM7yx0W',
+        :token_secret => 'XSDpwrfxRya6KHB3azNrtBKxG0hAjxJKJc8JGFRQhxQ' 
+      }
+      m = stub(:statuses => stub( :update! => 'go' ))
+      Grackle::Client.expects(:new).once.with(:auth => auth).returns(m)
+      MauTweeter.new.send(:tweet, stub(:message =>'give me pith or give me death'))
+    end
+    it 'tweets a message' do
+      m = mock "GrackleClient"
+      Grackle::Client.any_instance.expects(:statuses).once.returns(m)
+      m.expects(:update!).once
+      MauTweeter.new.send(:tweet, stub(:pithy_message =>'give me pith or give me death'))
+    end
+  end
+end
