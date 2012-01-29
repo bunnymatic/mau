b3a80940e4bc322eac4e407bcb3c01535f4069d6
Mr Rogers <jon@bunnymatic.com>
2012-01-28 19:49:00 -0800

    * remove config feedback emails  * put in backup list of emails in feedback mailer

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,12 +1,14 @@
 class FeedbackMailer < MauMailer
   
   def feedback(feedback)
+    emails = 'feedback@missionartistsunited.org','trish@trishtunney.com' #just in case
     if mailer_list.present?
-      @recipients  =  mailer_list.formatted_emails
-      @from        = 'noreply@missionartistsunited.org'
-      @subject     = "[MAU Feedback] #{feedback.subject}"
-      @sent_on     = Time.now
+      emails = mailer_list.formatted_emails
     end
+    @recipients  =  emails
+    @from        = 'noreply@missionartistsunited.org'
+    @subject     = "[MAU Feedback] #{feedback.subject}"
+    @sent_on     = Time.now
     @body[:feedback] = feedback
 
   end
