3cd22d24be5a367e3d76bd9077d0a7e18c8bb204
Mr Rogers <jon@bunnymatic.com>
2012-04-28 16:52:50 -0700

    * integrate links to show the calendar  * show only reception events on the calendar  * fix db config

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,7 +1,6 @@
 class CalendarController < ApplicationController
-
+  include EventsHelper
   layout 'mau2col'
-  
   def index
     @month = (params[:month] || (Time.zone || Time).now.month).to_i
     @year = (params[:year] || (Time.zone || Time).now.year).to_i
@@ -11,6 +10,8 @@ class CalendarController < ApplicationController
     start_d, end_d = Event.get_start_and_end_dates(@shown_month) # optionally pass in @first_day_of_week
     @events = Event.published.events_for_date_range(start_d, end_d)
     @event_strips = Event.create_event_strips(start_d, end_d, @events)
+
+    published_events, @events_by_month = fetch_published_events_by_month
   end
   
 end
