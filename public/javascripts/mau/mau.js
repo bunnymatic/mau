4a83eb5c9498498c0954d6bf39800bb116da5b31
Mr Rogers <jon@bunnymatic.com>
2012-07-28 17:49:59 -0700

    * added show/hide for sections in search page

--- public/javascripts/mau/mau.js
+++ public/javascripts/mau/mau.js
@@ -710,7 +710,6 @@ var TagMediaHelper = {
   T.APCache = {};
   T.Helpers = {
     find_thumb: function(apid) {
-      console.log('find thumb');
       var i = 0;
       if (T.ThumbList) {
         var n = T.ThumbList.length;
@@ -1602,13 +1601,73 @@ var TagMediaHelper = {
   });
   Event.observe(window, 'load', D.init);    
 
+  
+  /** search page */
   var Search = M.Search = M.Search || {};
-  Search.init = function() {
-    Search.init = function() {};
-  };
-  Event.observe(window, 'load', Search.init);
-    
+  Object.extend(Search, {
+    sprite_minus_dom: '<div class="sprite minus" alt="hide" />',
+    sprite_plus_dom: '<div class="sprite plus" alt="show" />',
+    init: function() {
+    /* expando based on 
 
+   Written by Jonathan Callahan - 
+   SourceCode: http://mazamascience.com/code/ShowHide/
+   Based On: http://onlinetools.org/tools/domcollapse/
+   Released under the MIT license:
+   
+   Copyright (c) 2008 Jonathan Callahan
+   
+   Permission is hereby granted, free of charge, to any person
+   obtaining a copy of this software and associated documentation
+   files (the "Software"), to deal in the Software without
+   restriction, including without limitation the rights to use,
+   copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following
+   conditions:
+   
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+   
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+   OTHER DEALINGS IN THE SOFTWARE.
+  */
+    
+      var triggers = $$('.column .trigger');
+      for (var i = 0, len = triggers.length; i < len; ++i) {
+        triggers[i].insert({ top: Search.sprite_minus_dom });
+        triggers[i].next().hide();
+        triggers[i].observe('click',Search.toggleTarget);
+      }
+      var expandeds = $$('.column .expanded');
+      for (var i = 0, len = expandeds.length; i < len; ++i) {
+        expandeds[i].insert({ top: Search.sprite_plus_dom });
+        expandeds[i].addClassName('trigger');
+        expandeds[i].observe('click',Search.toggleTarget);
+      }
+    },
+    toggleTarget:function(event) {
+      // NOTE:  Within an event handler, 'this' always refers to the element they are registered on.
+      var t = event.currentTarget;
+      if(!t.hasClassName('expanded')) {
+        t.addClassName('expanded');
+        t.down('div').replace( Search.sprite_plus_dom );
+        t.next().blindDown(M.BLIND_OPTS.down);
+      } else {
+        t.removeClassName('expanded');
+        t.down('div').replace( Search.sprite_minus_dom );
+        t.next().slideUp(M.BLIND_OPTS.up);
+      }
+    }
+  });
+    
+ Event.observe(window, 'load', Search.init);
 
   var E = M.Events = M.Events || {};
   E.init = function() {
