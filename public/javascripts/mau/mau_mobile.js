ecf56d322f825b7712c6aaf27cb7254f4dc29ac9
Mr Rogers <jon@bunnymatic.com>
2013-03-14 11:29:56 -0700

    * handle swipe on artist thumb page.

--- public/javascripts/mau/mau_mobile.js
+++ public/javascripts/mau/mau_mobile.js
@@ -1,5 +1,12 @@
-$(function() {
-  $('#thumbs').bind('touchy-swipe', function() {
-    $('.ui-page-active .apple_pagination .next_page .ui-link').trigger('click');
+$(document).bind('pageinit', function() {
+  $('.apple_pagination, #thumbs .thumb').bind('touchy-swipe', function(e, $target, data) {
+    var selector = {
+      right: '.ui-page-active .apple_pagination a.prev_page.ui-link',
+      left: '.ui-page-active .apple_pagination a.next_page.ui-link',
+    }[data.direction]
+    if (selector) {
+      $(selector).first().trigger('click');
+    }
   });
+    
 });
