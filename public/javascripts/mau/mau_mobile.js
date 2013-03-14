dbbba87afb717250d7e9061c04b243bb7fb10bb2
Mr Rogers <jon@bunnymatic.com>
2013-03-14 15:00:18 -0700

    * don't use touchy

--- public/javascripts/mau/mau_mobile.js
+++ public/javascripts/mau/mau_mobile.js
@@ -1,14 +1,5 @@
 var MAU = window.MAU = window.MAU || {};
 $(document).bind('pageinit', function() {
-  $('.apple_pagination, #thumbs .thumb').bind('touchy-swipe', function(e, $target, data) {
-    var selector = {
-      right: '.ui-page-active .apple_pagination a.prev_page.ui-link',
-      left: '.ui-page-active .apple_pagination a.next_page.ui-link',
-    }[data.direction]
-    if (selector) {
-      $(selector).first().trigger('click');
-    }
-  });
 
   $('.show_more a').bind('click', function(ev) {
     ev.preventDefault();
