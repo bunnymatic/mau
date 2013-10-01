11ccbc5e70cca32f7ecc77c184bd3f30b1e66937
Mr Rogers <jon@bunnymatic.com>
2013-10-01 01:52:03 -0700

    routing specs pass

--- spec/routing/artists_controller_routing_spec.rb
+++ spec/routing/artists_controller_routing_spec.rb
@@ -20,35 +20,35 @@ describe 'ArtistsController Routes' do
 
   describe "- route generation" do
     it "should map controller artists, id 10 and action show to /artists/10" do
-      route_for(:controller => "artists", :id => "10", :action => "show").should == "/artists/10"
+      (get '/artists/10').should route_to(:controller => "artists", :id => "10", :action => "show")
     end
     it "should map edit action properly" do
-      route_for(:controller => "artists", :action => "edit").should == "/artists/edit"
+      (get '/artists/edit').should route_to(:controller => "artists", :action => "edit")
     end
 
     it "should map users/index to artists" do
-      route_for(:controller => "artists", :action => "index").should == "/artists"
+      (get '/artists').should route_to(:controller => "artists", :action => "index")
     end
   end
 
   describe "- route recognition" do
     context "/artists/edit" do
       it "map get to artists controller edit method" do
-        params_from(:get, "/artists/edit").should == {:controller => "artists", :action => "edit" }
+        {:get, "/artists/edit"}.should route_to({:controller => "artists", :action => "edit" })
       end
     end
     context "/artists/10" do
       it "map PUT to update" do
-        params_from(:put, "/artists/10").should == {:controller => "artists", :action => "update", :id => "10" }
+        {:put, "/artists/10"}.should route_to({:controller => "artists", :action => "update", :id => "10" })
       end
       it "map GET to show" do
-        params_from(:get, "/artists/10").should == {:controller => "artists", :action => "show", :id => "10" }
+        {:get, "/artists/10"}.should route_to({:controller => "artists", :action => "show", :id => "10" })
       end
-      it "map POST to action == 10 (nonsense)" do
-        params_from(:post, "/artists/10").should == {:controller => "artists", :action => "10" }
+      it "map POST to update" do
+        {:post, "/artists/10"}.should route_to({:controller => "artists", :action => 'update' , :id => '10'})
       end
       it "map DELETE /artists/10 as destroy" do
-        params_from(:delete, "/artists/10").should == {:controller => "artists", :action => "destroy", :id => "10" }
+        {:delete, "/artists/10"}.should route_to({:controller => "artists", :action => "destroy", :id => "10" })
       end
     end
   end
