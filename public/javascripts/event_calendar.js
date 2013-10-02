73fa2233a69e9d9ee7d63737ab9ed257c175d9b0
Trish Tunney <trish@trishtunney.com>
2013-10-01 23:25:27 -0700

    jQuery update to event-calendar

--- public/javascripts/event_calendar.js
+++ public/javascripts/event_calendar.js
@@ -2,53 +2,29 @@
  * Smart event highlighting
  * Handles for when events span rows, or don't have a background color
  */
-Event.observe(window, "load", function() {
+
+ jQuery(document).ready(function() {
   var highlight_color = "#738294";
-  
+
   // replace >> << with raquo and laquo
-  $$('.ec-month-nav').each(function(el) {
-    el.innerHTML = el.innerHTML.replace('&lt;&lt; ', '&laquo; ').replace(' &gt;&gt;', ' &raquo;');
+  jQuery('.ec-month-nav').each(function() {
+    var elementHtml = jQuery(this).html();
+    elementHtml = elementHtml.replace('&lt;&lt; ', '&laquo; ').replace(' &gt;&gt;', ' &raquo;');
+    jQuery(this).html(elementHtml);
   });
 
   // highlight events that have a background color
-  $$(".ec-event-bg").each(function(ele) {
-    ele.observe("mouseover", function(evt) {
-      event_id = ele.readAttribute("data-event-id");
-      $$(".ec-event-"+event_id).each(function(el) {
-        el.setStyle({ backgroundColor: highlight_color });
-      });
-    });
-    ele.observe("mouseout", function(evt) {
-      event_color = ele.readAttribute("data-color");
-      event_id = ele.readAttribute("data-event-id");
-      $$(".ec-event-"+event_id).each(function(el) {
-        el.setStyle({ backgroundColor: event_color });
-      });
-    });
+  jQuery('.ec-event-bg').each(function() {
+    jQuery(this).bind('mouseover', function() { 
+      var eventId = jQuery(this).data('event-id')
+      var className = '.ec-event-' + eventId;
+      jQuery(className).css({backgroundColor: highlight_color});
+    })
+    jQuery(this).bind('mouseout', function() {
+      var event_color = jQuery(this).data('color')
+      var eventId = jQuery(this).data('event-id');
+      var className = '.ec-event-' + eventId;
+      jQuery(className).css({backgroundColor: event_color});
+    })
   });
-  
-  // highlight events that don't have a background color
-  $$(".ec-event-no-bg").each(function(ele) {
-    ele.observe("mouseover", function(evt) {
-      ele.setStyle({ color: "white" });
-      ele.select("a").each(function(link) {
-        link.setStyle({ color: "white" });
-      });
-      ele.select(".ec-bullet").each(function(bullet) {
-        bullet.setStyle({ backgroundColor: "white" });
-      });
-      ele.setStyle({ backgroundColor: highlight_color });
-    });
-    ele.observe("mouseout", function(evt) {
-      event_color = ele.readAttribute("data-color");
-      ele.setStyle({ color: event_color });
-      ele.select("a").each(function(link) {
-        link.setStyle({ color: event_color });
-      });
-      ele.select(".ec-bullet").each(function(bullet) {
-        bullet.setStyle({ backgroundColor: event_color });
-      });
-      ele.setStyle({ backgroundColor: "transparent" });
-    });
-  });
-});
+}); 
