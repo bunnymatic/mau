aa5677fc279d7bc48b4cd3028c805901e460d00c
Mr Rogers <jon@bunnymatic.com>
2013-10-17 00:10:21 -0700

    * dump unused feedback helper code

--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -1,26 +1,24 @@
-module FeedbacksHelper 
-  
-  def feedback_tab(options = {})
-    feedback_init({'position' => 'top'}.merge(options.stringify_keys))
-  end
-  
-  def feedback_init(options = {})
-    options = {
-      "position" => "null"
-    }.merge(options.stringify_keys)
-    
-    options['position'] = "'#{options['position']}'" unless options['position'].blank? || options['position'] == 'null'
-    content_tag 'script', :type => "text/javascript" do
-      "document.observe(\"dom:loaded\", function() { Feedback.init({tabPosition: #{options["position"]}}); });"
-    end  
-    
-  end
-  
-  def feedback_includes()
-    # included in "all" js
-    #javascript_include_tag('prototype.feedback.js')
-  end
-  
+module FeedbacksHelper
+
+  # def feedback_tab(options = {})
+  #   feedback_init({'position' => 'top'}.merge(options.stringify_keys))
+  # end
+
+  # def feedback_init(options = {})
+  #   options = {
+  #     "position" => "null"
+  #   }.merge(options.stringify_keys)
+
+  #   options['position'] = "'#{options['position']}'" unless options['position'].blank? || options['position'] == 'null'
+  #   content_tag 'script', :type => "text/javascript" do
+  #     "document.observe(\"dom:loaded\", function() { Feedback.init({tabPosition: #{options["position"]}}); });"
+  #   end
+
+  # end
+
+  # def feedback_includes
+  # end
+
   def feedback_link(text, options = {})
     xclass = ''
     if options.include?(:class)
