6a4bc0fb53de77b6ed20b68335ea8a332b1601d3
Mr Rogers <jon@bunnymatic.com>
2014-01-01 19:32:56 -0800

    feed controller cleanup (more to come)

--- app/models/feed_parser.rb
+++ app/models/feed_parser.rb
@@ -4,64 +4,29 @@ require 'rss/2.0'
 class FeedParser
 
   NUM_POSTS = 3
-  DESCRIPTION_LENGTH = 600
-  TITLE_LENGTH = 140
-  DESC_CLEANER = Regexp.new(/<\/?[^>]*>/)
 
-  attr_accessor :options, :url, :link
+  attr_accessor :options, :url, :link, :num_entries, :strip_tags, :include_date, :truncate, :css_class
 
   def initialize(url, link, opts)
     @url = URI.parse(url)
     @link = link
-    @options = {
-      :numentries => NUM_POSTS,
+    options = {
+      :num_entries => NUM_POSTS,
       :strip_tags => true,
       :include_date => false,
       :truncate => true,
       :css_class => ''
-    }.merge(opts)
-  end
-
-  def parse_entry(entry, link, css_class)
-    strip_tags = @options[:strip_tags]
-    include_date = @options[:include_date]
-    truncate = @options[:truncate]
-    if ! entry
-      return "<div class='feedentry'></div>"
-    end
-    # process entry if we want to.
-    if strip_tags
-      entry.description.gsub!(DESC_CLEANER," ")
-    end
-
-    entry.description.strip!
-    entry.title.strip!
-    title = entry.title
-    desc = entry.description
-    feed = "<div class='feedentry  #{css_class}'>"
-    if include_date
-      feed += "<span class='feeddate'>%s</span>" % entry.date
-    end
-
-    if truncate
-      title = title.truncate TITLE_LENGTH if title
-    end
-
-    if (!@link.include? 'twitter') && truncate
-      desc = desc.truncate DESCRIPTION_LENGTH
-    end
-
-    feed += "<div class='feedtitle'>%s</div>" % title if title
-    feed += "<div class='feedtxt'>%s</div>" % desc if desc
-    feed += "</div>"
-    feed
+    }.merge(opts.symbolize_keys)
+    @num_entries = options[:num_entries]
+    @strip_tags = options[:strip_tags]
+    @include_date = options[:include_date]
+    @truncate = options[:truncate]
+    @css_class = options[:css_class]
   end
 
   def fetch
-    numentries = @options[:numentries]
-
     logger = ::Rails.logger
-    logger.info("FeedsHelper: fetch/format %s posts." % numentries)
+    logger.info("FeedsHelper: fetch/format %s posts." % @num_entries)
     feedcontent = ''
     begin
       logger.info("FeedsHelper: open #{@url}")
@@ -86,25 +51,45 @@ class FeedParser
           hdr = "%s%s%s%s%s</div>" % [ startdiv, startlink, titlestr, icon, endlink]
           feedcontent += "<div class='feed-entries'>"
           feedcontent += hdr
-          num = [result.items.length, numentries].min - 1
-          result.items[0..num].each_with_index do |entry, idx|
+          num = [result.items.length, num_entries].min
+          result.items.first(num).each_with_index do |entry, idx|
             xtra_class = (0==(idx % 2)) ? "odd":"even"
             feedcontent += parse_entry(entry, @link, xtra_class)
           end
           feedcontent += "</div>"
         end
-        logger.info("FeedsHelper: done")
       end
     rescue OpenURI::HTTPError => http
-      logger.warn("FeedsHelper: failed to open/parse feed " + http.to_s)
-      logger.warn("FeedsHelper: skipping to the next")
+      puts "FeedsHelper: failed to open/parse feed " + http.to_s
+      puts "FeedsHelper: skipping to the next"
       feedcontent = ''
     rescue Exception => e
-      logger.warn("FeedsHelper: failed to open/parse feed " + e.to_s)
+      puts "FeedsHelper: failed to open/parse feed " + e.to_s
       raise
     end
     feedcontent
   end
+
+  private
+  def parse_entry(source_entry, link, css_class)
+
+    if ! source_entry
+      return "<div class='feedentry'></div>"
+    end
+
+    entry = FeedEntry.new(source_entry, link, @strip_tags, @truncate)
+
+    feed = "<div class='feedentry  #{css_class}'>"
+    if @include_date
+      feed += "<span class='feeddate'>%s</span>" % entry.date
+    end
+
+    feed += "<div class='feedtitle'>%s</div>" % entry.title if entry.title.present?
+    feed += "<div class='feedtxt'>%s</div>" % entry.description if entry.description.present?
+    feed += "</div>"
+    feed
+  end
+
 
   URLMATCH_TO_CLASS = {
     'facebook.com' => 'facebook',
@@ -117,8 +102,6 @@ class FeedParser
     'blogspot.com' => 'blogger',
     'deviantart.com' => 'deviantart',
     'flaxart.com' => 'flaxart' }.freeze
-
-
 
   def get_icon_class(url)
     # guess icon class based on url.
---
+++ spec/models/feed_entry_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe FeedEntry do
+
+  let(:source_url) { 'http://wordpress.com' }
+  let(:the_time) { Time.zone.now }
+  let(:description) { '<div>the <img src="img.jpg"/> <br/> description  </div>   ' }
+  let(:title) { '<div>the <h2>title  </h2>  </div>   ' }
+  let(:raw_entry) { OpenStruct.new(:title => title, :description => description, :date => the_time) }
+  let(:trunc) { false } 
+  let(:clean_desc) { true }
+  subject(:entry) { FeedEntry.new(raw_entry, source_url, clean_desc, trunc) }
+  
+  its(:title) { should eql title.strip }
+  its(:description) { should eql 'the     description' }
+  its(:date) { should eql the_time }
+
+  context 'when clean_desc is false' do
+    let(:clean_desc) { false }
+    its(:description) { should eql description.strip }
+  end
+
+  context 'when truncate is true' do
+    let(:trunc) { true }
+
+    context 'and it has a long title' do
+      let(:title) { Faker::Lorem.words(70).join(" ") }
+      it 'truncates the title' do
+        expect(subject.title).to have(FeedEntry::TITLE_LENGTH).characters
+      end
+    end
+     
+    context 'and the description is really long' do
+      let(:description) { Faker::Lorem.words(300).join(" ") }
+      it 'truncates the description' do
+        expect(subject.description).to have(FeedEntry::DESCRIPTION_LENGTH).characters 
+      end
+    end
+    
+  end
+
+end
