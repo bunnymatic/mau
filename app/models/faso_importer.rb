41852114118a3fdb0eac3e77e47b2f25dcff1946
Mr Rogers <jon@bunnymatic.com>
2014-01-05 20:45:06 -0800

    refactor scammer import

---
+++ app/models/faso_importer.rb
@@ -0,0 +1,55 @@
+class FasoImporter
+
+  include Enumerable
+
+  def initialize
+    @headers = nil
+  end
+
+  def scammers 
+    @scammers ||= fetch
+  end
+
+  def uri 
+    @uri ||= URI.parse("https://api.faso.com/1/scammers?key=2386ad2c89aa40dfa0ce90e868797a33&format=pipe")
+  end
+
+  def make_request
+    @request ||=
+      begin
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        req = Net::HTTP::Get.new(uri.request_uri)
+        http.request(req)
+      end
+  end
+
+  def fetch
+    # import data from FASO database
+    resp = make_request
+    resp.body.split("\n").map(&:chomp).map do |row|
+      entry = parse_row(row)
+      Scammer.new( :name => entry['name_used'], :faso_id => entry['id'], :email => entry['email']) if entry
+    end.compact.uniq
+  end
+
+  def each(&block)
+    scammers.each(&block)
+  end
+
+  private
+  def parse_row(row)
+    if !@headers
+      @headers = pipe_split(row)
+      nil
+    else
+      Hash[@headers.zip(pipe_split(row))]
+    end
+  end
+
+  def pipe_split(row)
+    (row.split '|').map{|entry| entry.gsub(/^"/, '').gsub(/"$/, '')}
+  end
+
+end
--- app/models/scammer.rb
+++ app/models/scammer.rb
@@ -20,27 +20,8 @@ class Scammer < ActiveRecord::Base
   validates_length_of       :email,    :within => 6..100 #r@a.wk
 
   def self.importFromFASO
-    # import data from FASO database
-    uri = URI.parse("https://api.faso.com/1/scammers?key=2386ad2c89aa40dfa0ce90e868797a33&format=pipe")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    req = Net::HTTP::Get.new(uri.request_uri)
-    resp = http.request(req)
-    headers = []
-    scammers = []
-    resp.body.split("\n").each do |row|
-      row.chomp!
-      entries = (row.split '|').map{|entry| entry.gsub(/^"/, '').gsub(/"$/, '')}
-
-      if headers.empty?
-        headers = entries
-      else
-        v = Hash[headers.zip( entries ) ]
-        scammers.push(Scammer.new( :name => v['name_used'], :faso_id => v['id'], :email => v['email']))
-      end
-    end
-    scammers.each do |s|
+    importer = FasoImporter.new
+    importer.scammers.each do |s|
       s.save if s.valid?
     end
   end
