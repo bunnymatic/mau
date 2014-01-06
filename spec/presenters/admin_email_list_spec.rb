99f446291b56164a133722137305b4469c88fd77
Mr Rogers <jon@bunnymatic.com>
2014-01-05 16:30:05 -0800

    Add CSV methods to admin email list presenter
    * add csv methods
    * use them

--- spec/presenters/admin_email_list_spec.rb
+++ spec/presenters/admin_email_list_spec.rb
@@ -9,6 +9,7 @@ describe AdminEmailList do
 
   its(:display_title) { should eql "Activated [#{Artist.active.count}]" }
   its("artists.to_a") { should eql Artist.active.to_a }
+  its(:csv_filename) { should eql 'email_active.csv' }
 
   it 'includes the normal lists' do
     %w(all active pending fans no_profile no_images).each do |k|
@@ -24,6 +25,9 @@ describe AdminEmailList do
 
   context 'listname is fans' do
     let(:listname) { 'fans' }
+
+    its(:csv_filename) { should eql 'email_fans.csv' }
+
     it 'assigns a list of fans emails when we ask for the fans list' do
       emails.length.should eql MAUFan.all.count
     end
@@ -36,6 +40,9 @@ describe AdminEmailList do
 
   context 'listname is pending' do
     let(:listname) { 'pending' }
+
+    its(:csv_filename) { should eql 'email_pending.csv' }
+
     it 'assigns a list of pending emails when we ask for the fans list' do
       email_list.artists.to_a.should eql Artist.pending.to_a
     end
@@ -61,20 +68,16 @@ describe AdminEmailList do
     end
   end
 
+  context 'with multiple os tags' do
+    let(:ostags) { Conf.open_studios_event_keys.map(&:to_s).sample(2) }
+    let(:listname) { ostags }
+
+    its(:csv_filename) { should eql 'email_' + listname.join("_") + ".csv" }
+
+    it 'returns emails that have been in both open studios' do
+      expected = Artist.all.select{|a| a.os_participation[ostags.first]}.map(&:email) |
+        Artist.all.select{|a| a.os_participation[ostags.last]}.map(&:email)
+      expect(emails.map(&:email).sort).to eql expected.sort
+    end
+  end
 end
-
-
-
-      #   it 'the emails list is an intersection of all artists in those open studios groups' do
-      #     emails = Artist.all.select{|a| a.os_participation['201004']}.map(&:email) |
-      #       Artist.all.select{|a| a.os_participation['201010']}.map(&:email)
-      #     emails.should eql assigns(:emails).map{|em| em[:email]}
-      #   end
-      #   it 'the emails list is an intersection of all artists in those open studios groups' do
-      #     emails = Artist.all.select{|a| a.os_participation['201004']}.map(&:email) |
-      #       Artist.all.select{|a| a.os_participation['201010']}.map(&:email)
-      #     emails.should eql assigns(:emails).map{|em| em[:email]}
-      #     emails.each do |em|
-      #       assert_select '.email_results table tbody tr td', /#{em}/
-      #     end
-      #   end
