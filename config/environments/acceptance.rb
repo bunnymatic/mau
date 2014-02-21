7842bfb29d08998a11c51ee2b728441736f2b6c0
Mr Rogers <jon@bunnymatic.com>
2014-02-20 23:58:30 -0800

    show errors on acceptance

--- config/environments/acceptance.rb
+++ config/environments/acceptance.rb
@@ -8,7 +8,7 @@ Mau::Application.configure do
 
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching             = true
 
   # See everything in the log (default is :info)
