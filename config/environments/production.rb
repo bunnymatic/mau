d56357b6362c197e6da8f606ffb8b938cae5006c
Mr Rogers <jon@bunnymatic.com>
2014-03-01 12:25:18 -0800

    Move feeds to jquery/coffeescript
    * cleanup refs to mau_feeds

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Mau::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( mau.css mau-ie.css mau-ie7.css mau-safari.css jquery.mobile-1.3.0.min.css thirdparty/excanvas.compiled.js gmaps_google.js mau/mau_gmap.js thirdparty/autocomplete.min.js thirdparty/flotr/0.2.0/flotr.js mau/mau_feeds.js thirdparty/underscore.min.js mau/query_string_parser.js mau/mau_mobile.js )
+  config.assets.precompile += %w( mau.css mau-ie.css mau-ie7.css mau-safari.css mau-admin.css jquery.mobile-1.3.0.min.css thirdparty/excanvas.compiled.js gmaps_google.js mau/mau_gmap.js thirdparty/autocomplete.min.js mau/query_string_parser.js mau/mau_mobile.js )
 
   config.action_mailer.default_url_options = {
     :host => 'www.missionartistsunited.org'
