535e2d75146121d016d4f52ce37f727f7c641c3e
Mr Rogers <jon@bunnymatic.com>
2014-04-13 18:44:32 -0700

    whip

--- app/assets/javascripts/mau/mau_notes.js
+++ app/assets/javascripts/mau/mau_notes.js
@@ -8,60 +8,12 @@ avoids 'mailto' links
 MAU.NotesMailer = Class.create();
 var FormConstructors = function() {
 
-  var input_button = function(opts) {
-    var o = _.extend(opts || {}, { "class": 'formbutton', type: 'submit', value: 'send' });
-    return new Element('input', o);
-  };
-
-  var email_fields = function(email) {
-    email = email || '';
-    return(
-      [
-        new Element('label').update('Email'),
-        new Element('div').insert(new Element('input', { type: 'text',
-                                                         id: 'email',
-                                                         name: 'feedback_mail[email]',
-                                                         value: email})),
-        new Element('label').update('Confirm Email'),
-        new Element('div').insert(new Element('input', { type: 'text',
-                                                         id: 'email_confirm',
-                                                         name: 'feedback_mail[email_confirm]',
-                                                         value: email })) ]
-    );
-  };
-
-  this.types = ['inquiry', 'email_list', 'feed_submission', 'help'];
+  this.types = ['inquiry', 'feed_submission', 'help'];
 
   this.inquiry = {
     title: 'General Inquiry',
     render: function() {
-      var el = new Element('div');
-      el.innerHTML = "We love to hear from you.  Please let us know your thoughts,"+
-        " questions, rants.  We'll do our best to respond in a timely manner.";
-
-      var inputs = new Element('ul');
-      var entries = [];
-
-      MAU.Cookie.init({name:'mau'});
-      var email = MAU.Cookie.getData('email') || '';
-      entries.push( email_fields() );
-      entries.push( [
-        new Element('label').update('Question'),
-        new Element('div').insert(new Element('textarea', { columns: 80,
-                                                            rows: 7,
-                                                            id: 'inquiry',
-                                                            name: 'feedback_mail[inquiry]' })) ]);
-      entries.push( [ input_button() ] );
-
-      $(entries).each(function(entry) {
-        var li = new Element('li');
-        $(entry).each(function(chunk) {
-          li.insert(chunk);
-        });
-        inputs.insert(li);
-      });
-      el.insert(inputs);
-      return $(el);
+      document.getElementById('feedback-help').innerHTML
     }
   };
 
@@ -78,36 +30,7 @@ var FormConstructors = function() {
       return document.getElementById('feedback-help').innerHTML
     }
   };
-
-  this.email_list = {
-    title: 'Join our mailing list',
-    render: function() {
-      var el = new Element('div');
-      el.innerHTML = "Awesome!  We'll notify you of upcoming MAU events.  "+
-        "We hate spam just like you do so the only things you'll be apprised"+
-        " of will be great!  Enter your email twice below and we'll put you "+
-        "on the list.";
-
-      var inputs = new Element('ul');
-      var entries = [];
-
-      MAU.Cookie.init({name:'mau'});
-      var email = MAU.Cookie.getData('email') || '';
-      entries.push( email_fields() );
-      entries.push( [ input_button() ] );
-
-      $(entries).each(function(entry) {
-        var li = new Element('li');
-        $(entry).each(function(chunk) {
-          li.insert(chunk);
-        });
-        inputs.insert(li);
-      });
-      el.insert(inputs);
-      return $(el);
-    },
-  };
-};
+}
 
 Object.extend(MAU.NotesMailer.prototype, {
   defaults: {
@@ -123,29 +46,20 @@ Object.extend(MAU.NotesMailer.prototype, {
     }
   },
   close: function(ev) {
-    $$(this._parent_class(true)).each(function(el) {
-      if (el) {
-        el.fade({duration:0.3, afterFinish: function() {
-          if (el) {
-            el.remove();
-          }
-        }});
-      }
+    ev.preventDefault();
+    jQuery(this._parent_class(true)).fade(300, function() {
+      jQuery(this).remove();
     });
-    if (ev) {
-      ev.stopPropagation();
-    }
-    return false;
   },
   insert: function(ev) {
     var browser = new MAU.BrowserDetect()
     var xpos = 0, ypos = 0;
     if (ev) {
-      xpos = ev.pointerX();
-      ypos = ev.pointerY();
+      xpos = ev.clientX;
+      ypos = ev.clientY;
     }
     var _that = this;
-    if ($$(this._parent_class(true)).length === 0 ) {
+    if (jQuery(_that._parent_class(true)).length === 0 ) {
       var note_class = _that.options.note_class;
       var formbuilder = _that.form_builders[note_class];
       var inner = '';
@@ -206,16 +120,12 @@ Object.extend(MAU.NotesMailer.prototype, {
     }
   },
   initialize: function(selector, opts) {
-    this.options = Object.extend({},this.defaults);
-    this.options = Object.extend(this.options, opts);
+    this.options = _.extend({},this.defaults, opts)
     this.selector = selector;
     if (this.options.note_class in this.form_builders) {
       var _that = this;
-      $$(this.selector).each(function(el) {
-        el.observe('click', function(ev) {
-          _that.insert(ev);
-          return false;
-        });
+      jQuery(this.selector).bind('click', function(ev) {
+        _that.insert();
       });
     }
   }
