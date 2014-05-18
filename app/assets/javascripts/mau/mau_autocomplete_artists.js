7f9d3de9481d0b416cd688ac031ef05c70fcf4bb
Mr Rogers <jon@bunnymatic.com>
2014-05-18 00:28:06 -0700

    fix autocomplete on event submit

--- app/assets/javascripts/mau/mau_autocomplete_artists.js
+++ app/assets/javascripts/mau/mau_autocomplete_artists.js
@@ -1,27 +1,20 @@
 jQuery(function() {
-  artistSuggestFormatter = function(item) { return item.value; }
-  var event_artist_list = jQuery('#event_artist_list')
+  //artistSuggestFormatter = function(item) { return item.value; }
+  var event_artist_list = jQuery('#event_artist_list');
   if (event_artist_list.length) {
-    event_artist_list.select2({
-      multiple: true,
-      placeholder: "find artists by name",
-      minimumInputLength: 3,
-      ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
-        url: "/artists/suggest",
-        dataType: 'json',
-        data: function (query, result) {
-          return {
-            q: query, // search term
-            page_limit: 10,
-          };
-        },
-        results: function (data, result) { // parse the results into the format expected by Select2.
-          // since we are using custom formatting functions we do not need to alter remote JSON data
-          return {results: data}
-        }
-      },
-      formatResult: artistSuggestFormatter,
-      formatSelection: artistSuggestFormatter
-    });
+    var ajaxParams = {
+      url: "/artists/suggest",
+      dataType: 'json',
+      success: function(data) {
+        tags = _.map(data, function(item) { return item.value }),
+        console.log(tags);
+        event_artist_list.select2({
+          tags: tags,
+          minimumInputLength: 3,
+          multiple: true
+        });
+      }
+    };
+    jQuery.ajax(ajaxParams);
   }
 });
