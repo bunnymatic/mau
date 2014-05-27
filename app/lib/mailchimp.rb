5337431a1b3ac482fc7db2ef85c457e8966a582f
Mr Rogers <jon@bunnymatic.com>
2014-05-27 08:59:53 -0700

    working on mail chimp fixes

--- app/lib/mailchimp.rb
+++ app/lib/mailchimp.rb
@@ -1,13 +1,8 @@
 module MailChimp
 
   API_KEY = Conf.mailchimp_api_key
-  if Rails.env == 'production'
-    FANS_LIST = 'Mission Artists United Events Only'
-    ARTISTS_LIST = 'Mission Artists United List'
-  else
-    FANS_LIST = 'test list fans'
-    ARTISTS_LIST = 'test list artists'
-  end
+  FAN_LIST_ID = Conf.mailchimp_fan_list_id
+  ARTIST_LIST_ID = Conf.mailchimp_artist_list_id
 
   def subscribe_and_welcome
     raise "Config:mailchimp_key not set" if API_KEY.blank?
@@ -23,10 +18,6 @@ module MailChimp
     'activated_at'        => 'CREATED'
   }
 
-  def mailchimp_list_name
-    (self.class == Artist) ? ARTISTS_LIST : FANS_LIST
-  end
-
   def mailchimp_additional_data
     attrs = attributes.keys.select{|k| ATTRIBUTE_CONVERSIONS.keys.include? k}
     conv = attrs.map{|k| [ATTRIBUTE_CONVERSIONS[k],send(k)] }
@@ -35,18 +26,8 @@ module MailChimp
 
   # http://www.mailchimp.com/api/rtfm/listsubscribe.func.php
 
-  # extract the list_id for the given list_name
-  def mailchimp_list_id(list_name)
-    gb       = Gibbon.new(API_KEY)
-    response = gb.lists()
-    lists    = response['data']
-    list     = lists.find { |list| list['name'] == list_name }
-    raise "List with name '#{list_name}' not found: " + response.to_s if list.nil?
-    list["id"]
-  end
-
   def mailchimp_list_subscribe
-    list_id  = mailchimp_list_id( mailchimp_list_name )
+    list_id = (self.class == Artist) ? ARTIST_LIST_ID : FAN_LIST_ID
 
     email_parts   = email.split('@')
     email_address = CGI::escape(email_parts[0]) + '@' + email_parts[1]
