fa2f84933cbeefc4564fcb3d80f4a4df527b5514
Mr Rogers <jon@bunnymatic.com>
2014-11-02 12:36:05 -0800

    dump fixtures from api controller

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,8 +1,17 @@
 require 'spec_helper'
 
 describe ApiController do
-  fixtures :roles_users, :users, :roles, :artist_infos,
-    :studios, :art_pieces, :media, :art_piece_tags
+
+  let(:studio) { FactoryGirl.create(:studio) }
+  let(:artists) { 
+    FactoryGirl.create_list(:artist, 2, :with_art, studio: studio, number_of_art_pieces: 3) +
+      FactoryGirl.create_list(:artist, 2, :with_art, number_of_art_pieces: 3) +
+        FactoryGirl.create_list(:artist, 2, :with_art, state: 'suspended')
+  }
+  let(:art_pieces) { artists.map(&:art_pieces).flatten }
+  let(:art_piece) { art_pieces.first }
+  let(:artist) { artists.first }
+
   before do
     Rails.cache.stub(:read => nil)
   end
@@ -39,7 +48,6 @@ describe ApiController do
   context 'bad requests' do
     [['bogus'], ['bogus',1],
      ['artists','b','c','d'],  ['artists','2','edit'],
-     ['studios', ActiveRecord::Fixtures.identify(:blue),'destroy'],
      ['hash']].each do |params_path|
       context "given #{params_path.inspect} as input parameters" do
         before do
@@ -54,6 +62,12 @@ describe ApiController do
         JSON.parse(response.body)['message'].should match 'Unable to find the record'
         JSON.parse(response.body)['message'].should match typ
       end
+    end
+    context "given a studio destroy path as input parameters" do
+      before do
+        get :index, :path => ['studios',studio.id,'destroy']
+      end
+      it_should_behave_like 'error responses'
     end
   end
 
@@ -85,15 +99,15 @@ describe ApiController do
 
   context 'given [artists, artist_id] as input parameters' do
     before do
-      get :index, :path => ['artists', users(:jesseponce).id]
+      get :index, :path => ['artists', artist.id]
       @resp = JSON.parse(response.body)
     end
     it_should_behave_like 'good responses'
     it 'returns the artist we asked for' do
       @resp.should be_a_kind_of Hash
       @resp['artist'].should be_a_kind_of Hash
-      @resp['artist']['id'].should eql users(:jesseponce).id
-      @resp['artist']['firstname'].should eql users(:jesseponce).firstname
+      @resp['artist']['id'].should eql artist.id
+      @resp['artist']['firstname'].should eql artist.firstname
     end
   end
 
@@ -112,85 +126,92 @@ describe ApiController do
 
   context 'given [studios, studio_id] as input parameters' do
     before do
-      get :index, :path => ['studios', studios(:s1890).id]
+      studio
+      get :index, :path => ['studios', studio.id]
       @resp = JSON.parse(response.body)
     end
     it_should_behave_like 'good responses'
     it 'returns the studio we asked for' do
       @resp.should be_a_kind_of Hash
       @resp['studio'].should be_a_kind_of Hash
-      @resp['studio']['id'].should eql studios(:s1890).id
-      @resp['studio']['name'].should eql studios(:s1890).name
+      @resp['studio']['id'].should eql studio.id
+      @resp['studio']['name'].should eql studio.name
     end
   end
 
   context 'given /studios/studio_id as the path' do
     before do
-      get :index, :path => "studios/#{studios(:s1890).id}"
+      studio
+      get :index, :path => "studios/#{studio.id}"
       @resp = JSON.parse(response.body)
     end
     it_should_behave_like 'good responses'
     it 'returns the studio we asked for' do
       @resp.should be_a_kind_of Hash
       @resp['studio'].should be_a_kind_of Hash
-      @resp['studio']['id'].should eql studios(:s1890).id
-      @resp['studio']['name'].should eql studios(:s1890).name
+      @resp['studio']['id'].should eql studio.id
+      @resp['studio']['name'].should eql studio.name
     end
   end
 
   context 'given [art_pieces] as input parameters' do
     before do
+      artists
       get :index, :path => ['art_pieces']
       @resp = JSON.parse(response.body)
     end
     it_should_behave_like 'good responses'
     it 'returns a list of art_pieces' do
       @resp.should be_a_kind_of Array
-      @resp.count.should eql 11
+      @resp.count.should eql 12
       @resp.all? {|s| s.has_key? 'art_piece'}.should be_true, 'All items do not have the "art_piece" key'
     end
   end
 
   context 'given [art_pieces, art_piece_id] as input parameters' do
     before do
-      get :index, :path => ['art_pieces', art_pieces(:hot).id]
+      get :index, :path => ['art_pieces', art_piece.id]
       @resp = JSON.parse(response.body)
     end
     it_should_behave_like 'good responses'
     it 'returns the art_piece we asked for' do
       @resp.should be_a_kind_of Hash
       @resp['art_piece'].should be_a_kind_of Hash
-      @resp['art_piece']['id'].should eql art_pieces(:hot).id
-      @resp['art_piece']['title'].should eql art_pieces(:hot).title
+      @resp['art_piece']['id'].should eql art_piece.id
+      @resp['art_piece']['title'].should eql art_piece.title
     end
   end
 
   context 'given art_pieces, ids as input params' do
     before do
+      artists
       get :index, :path => ['art_pieces', 'ids']
       @resp = JSON.parse(response.body)
     end
     it_should_behave_like 'good responses'
     it 'returns the all art piece ids' do
       @resp.should be_a_kind_of Array
-      @resp.count.should eql 11
+      @resp.count.should eql 12
     end
   end
 
   context 'get parameter from object' do
     before do
-      get :index, :path => ['studios', studios(:blue).id.to_s, 'name']
+      studio
+      get :index, :path => ['studios', studio.id.to_s, 'name']
       @resp = JSON.parse(response.body)
     end
     it_should_behave_like 'good responses'
     it 'returns only the studio name' do
+      puts @resp
       @resp.keys.should include 'name'
-      @resp.should eql({'name' => 'The Blue Studio'})
+      @resp.should eql({'name' => studio.name})
     end
   end
 
   context 'get art piece of an inactive artist' do
     before do
+      artists
       inactive = (Artist.all - Artist.active).last
       get :index, :path => ['art_pieces', inactive.art_pieces.first.id]
     end
