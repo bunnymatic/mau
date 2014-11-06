f10ecde481b97a7408ad7cc5137c9793b8d825fe
Mr Rogers <jon@bunnymatic.com>
2014-11-05 22:43:22 -0500

    add missing indy studio model

---
+++ app/models/independent_studio.rb
@@ -0,0 +1,62 @@
+class IndependentStudio
+  extend  ActiveModel::Naming
+  extend  ActiveModel::Translation
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  
+  attr_reader :id, :name, :street, :city, :state, :image_height, :image_width, :cross_street, :phone
+
+  class ImageContainer
+    def initialize(img)
+      @img = img
+    end
+    def url(*args)
+      @img
+    end
+  end
+
+  def initialize(*args)
+    @id = 0
+    @name = 'Independent Studios'
+    @street = "The Mission District"
+    @city = "San Francisco"
+    @state = "CA"
+    @profile_image = ImageContainer.new("independent-studios.jpg")
+    @image_height = 1
+    @image_width = 1
+    @cross_street = nil
+    @phone = nil
+  end
+
+  def artists
+    @artists ||= Artist.active.where(["studio_id = 0 or studio_id is NULL"])
+  end
+
+  def url
+    @profile_image.url
+  end
+
+  def get_profile_image(*args)
+    @profile_image
+  end
+
+  def persisted?
+    true
+  end
+
+  def save
+    true
+  end
+
+  def save!
+    self
+  end
+
+  def map_link
+    nil
+  end
+
+  def profile_image?
+    true
+  end
+end
