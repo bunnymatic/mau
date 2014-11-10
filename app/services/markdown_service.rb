90eec15b7188322b3aae739652eb13e1b827ed1c
Mr Rogers <jon@bunnymatic.com>
2014-11-09 20:06:01 -0800

    strip script tags from markdown

--- app/services/markdown_service.rb
+++ app/services/markdown_service.rb
@@ -1,7 +1,11 @@
 class MarkdownService
-
-  def self.markdown(discount, markdown_opts = :smart)
-    RDiscount.new(discount || '', markdown_opts).to_html.html_safe
+  def self.cleanse(str)
+    doc = Nokogiri::HTML::DocumentFragment.parse(str)
+    doc.xpath(".//script").remove
+    doc.to_s
   end
 
+  def self.markdown(discount, markdown_opts = :smart)
+    cleanse(RDiscount.new(discount || '', markdown_opts).to_html).html_safe
+  end
 end
---
+++ spec/services/markdown_service_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe MarkdownService do
+  it "converts markdown to html" do
+    marked_down = MarkdownService.markdown("# one\n## two")
+    expect(marked_down).to include "<h1>one</h1>"
+    expect(marked_down).to include "<h2>two</h2>"
+  end
+
+  it "removes script tags" do
+    marked_down = MarkdownService.markdown("# one\n<h2>two</h2><script>alert('yo');</script>")
+    expect(marked_down).to include "<h1>one</h1>"
+    expect(marked_down).to include "<h2>two</h2>"
+    expect(marked_down).to_not include "script"
+  end
+end
