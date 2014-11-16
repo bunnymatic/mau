d0bc34db22b2262caf7b0f833beaaa3eda05c23f
Mr Rogers <jon@bunnymatic.com>
2014-11-16 12:17:08 -0800

    bump asset version

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,7 @@ module Mau
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.2'
 
     config.i18n.enforce_available_locales = true
 
