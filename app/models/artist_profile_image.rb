bf2e33434de2992d924e1f1ce8a1cc6fa0864177
Mr Rogers <jon@bunnymatic.com>
2014-12-30 10:23:35 -0800

    reinstate dummy image for favorites

--- app/models/artist_profile_image.rb
+++ app/models/artist_profile_image.rb
@@ -1,8 +1,17 @@
 class ArtistProfileImage < ProfileImage
 
+  DEFAULT_PROFILE_IMAGES = {
+    "medium" => '/images/s_default-profile-img-not-me.png',
+    "small" => '/images/s_default-profile-img-not-me.png',
+    "thumb" => '/images/t_default-profile-img-not-me.png'
+  }
+
   def self.get_path(artist, size="medium")
     # get path for image of size
     # size should be either "thumb","medium"
+    if !artist || artist.profile_image.nil?
+      return DEFAULT_PROFILE_IMAGES[size.to_s]
+    end
     dir = "/artistdata/" + artist.id.to_s() + "/profile/"
     fname = File.basename(artist.profile_image)
     ImageFile.get_path(dir, size.to_s, fname)
