5ff7c304341051c9d0e4b9996fefa31e13ca4b38
Mr Rogers <jon@bunnymatic.com>
2015-03-19 00:08:59 -0700

    style the recently added art header

--- app/presenters/art_sampler.rb
+++ app/presenters/art_sampler.rb
@@ -1,14 +1,23 @@
 class ArtSampler
 
   NUM_IMAGES = 15 - 2
-  def pieces
+  def initialize(*args)
+    super
+  end
+
+  def pieces 
     get_random_pieces.map{|piece| ArtPiecePresenter.new(piece)}
   end
 
+  def include_open_studios?
+    @include_open_studios ||= (Artist.open_studios_participants.count > 10)
+  end
+  
   private
   def get_random_pieces
     # get random set of art pieces and draw them
     ArtPiece.includes(:artist).where("users.state" => :active).order('rand()').limit(NUM_IMAGES)
+
   end
 
 end
