37f71772d87cdc8ee92467533db0b2ed376e45b7
Mr Rogers <jon@bunnymatic.com>
2015-05-11 20:41:21 -0400

    wip

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -5,3 +5,19 @@ end
 Then(/^I see art newly added to the system$/) do
   expect(page).to have_selector('#new-art .square')
 end
+
+When(/^I fill in the inquiry form$/) do
+  within '.popup-text' do
+    fill_in 'Email', with: 'joe@example.com'
+    fill_in 'Confirm Email', with: "joe@example.com"
+    fill_in 'Your Question', with: "what is the meaning of life?"
+  end
+end
+
+Then(/^I see that my inquiry was submitted$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the system knows that my inquiry was submitted$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
