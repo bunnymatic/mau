d3a0f09c2855d0a04d3deee3c5545571aaabaf06
Mr Rogers <jon@bunnymatic.com>
2015-05-15 19:56:52 -0700

    better handling of markdown edit

--- app/assets/javascripts/mau/mau_discount.js
+++ app/assets/javascripts/mau/mau_discount.js
@@ -1,15 +1,28 @@
+/*global    */
 var MAU = window.MAU = window.MAU || {};
 
 jQuery(function() {
-  // for test page
-  jQuery('#process_markdown_btn').bind('click', function() {
-    var markdown = jQuery('#input_markdown, #cms_document_article');
+  var markItDown = function( textarea, output ) {
+    var markdown = $(textarea);
     var txt = markdown.val() || '## no markdown to process';
     var params = {
       markdown: txt,
       authenticity_token:unescape(authenticityToken)
     };
-    jQuery('#processed_markdown').load('/admin/discount/markup', params)
+    jQuery(output).load('/admin/discount/markup', params)
+  };
+
+  jQuery('#process_markdown_btn').bind('click', function() {
+    markItDown('#input_markdown, #cms_document_article', '#processed_markdown')
+  });
+
+  jQuery('#cms_document_article').on('change', function() {
+    MAU.Utils.debounce(
+      function() {
+        markItDown('#input_markdown, #cms_document_article', '#processed_markdown')
+      },
+      250,false
+    )();
   });
 
 });
