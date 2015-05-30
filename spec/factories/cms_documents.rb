57578cf697c45ebce30cacb024b2ec515e99f5d4
Mr Rogers <jon@bunnymatic.com>
2015-05-30 00:22:20 -0700

    add search to mobile nav [#95681048]

--- spec/factories/cms_documents.rb
+++ spec/factories/cms_documents.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :cms_document do
-    page { Faker::Files.dir }
+    page { "page_#{Faker::Files.dir}" }
     section { Faker::Files.dir }
     article { "# this is an h1 header\n\n## this is an h2 header\n\n" + Faker::Lorem.paragraphs(2).join("\n") }
   end
