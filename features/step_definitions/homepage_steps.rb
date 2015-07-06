3549c60423a7cf9fa867ca6055ee9772450d2f35
Mr Rogers <jon@bunnymatic.com>
2015-07-05 18:11:11 -0700

    clean out remnants of "new_art"

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -2,7 +2,4 @@ Then(/^I see some of the art that's in the system$/) do
   expect(page).to have_selector('#sampler .sampler__thumb')
 end
 
-Then(/^I see art newly added to the system$/) do
-  expect(page).to have_selector('#new-art .square')
-end
 
