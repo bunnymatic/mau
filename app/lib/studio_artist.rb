0cd1be9725407c97f643d617141f7902393accaf
Mr Rogers <jon@bunnymatic.com>
2015-07-30 08:32:23 -0700

    remove dummy puts

--- app/lib/studio_artist.rb
+++ app/lib/studio_artist.rb
@@ -10,7 +10,6 @@ class StudioArtist
   end
 
   def unaffiliate
-    puts "artist_roles.count", artist_roles.count
     if artist_is_in_studio?
       @artist.update_attribute(:studio_id, nil)
       artist_roles.select(&:is_manager_role?).each(&:destroy)
