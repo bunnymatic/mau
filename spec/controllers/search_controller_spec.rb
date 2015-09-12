2c0ce90b0afe94df7235141cdad7f80b8a3535c5
Mr Rogers <jon@bunnymatic.com>
2015-09-12 16:17:54 -0700

    trim search controller specs

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -29,55 +29,12 @@ describe SearchController do
   describe "#index" do
     context "finding by studio" do
       before do
-        get :index, studios: studios_search.map(&:id), keywords: studio_artist_name_match
+        get :index, q: studio_artist_name_match
       end
-      it 'returns results' do
-        expect(assigns(:paginator).items).to be_present
-      end
-      it 'checks the studios you searched for' do
-        assigns(:query).studios.should have(2).studios
-      end
-    end
-
-    context "finding by medium" do
-      before do
-        get :index, mediums: media_search.map(&:id), keywords: studio_artist_name_match
-      end
-      it 'returns results' do
-        expect(assigns(:paginator).items).to be_present
-      end
-      it 'checks the media you searched for' do
-        assigns(:query).mediums.should have(2).media
+      it 'returns success' do
+        expect(response).to be_success
       end
     end
   end
 
-  describe '#fetch' do
-    before do
-      @artist = FactoryGirl.create(:artist, :active, :with_art, nomdeplume: 'Fancy Pants')
-    end
-    context 'simple keyword query' do
-      before do
-        post :fetch, keywords: 'fancy pants'
-      end
-      it "returns some results" do
-        assigns(:paginator).items.should have_at_least(1).art_piece
-      end
-      it "sets the correct page" do
-        assigns(:paginator).current_page.should eql 0
-      end
-      it "sets the correct result count" do
-        assigns(:paginator).count.should eql ArtPiece.find_all_by_artist_id(@artist.id).count
-      end
-    end
-
-    context 'with per_page set' do
-      before do
-        post :fetch, keywords: 'name1', per_page: 48
-      end
-      it 'resets the per_page to something reasonable' do
-        assigns(:query).per_page.should_not eql 48
-      end
-    end
-  end
 end
