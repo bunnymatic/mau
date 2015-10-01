b73fec41158909e42cb3c618b2c5275dc81da089
Mr Rogers <jon@bunnymatic.com>
2015-10-01 08:05:40 -0700

    fix issue on studios list page

--- app/models/studio_image.rb
+++ app/models/studio_image.rb
@@ -1,19 +1,19 @@
 class StudioImage < ProfileImage
 
-  # def self.get_path(studio, size="medium")
-  #   if studio.photo?
-  #     return studio.photo(size)
-  #   end
-  #   # get path for image of size
-  #   # size should be either "thumb","small", or "medium"
-  #   dir = "/studiodata/" + studio.id.to_s() + "/profile/"
-  #   if studio.profile_image?
-  #     fname = File.basename(studio.profile_image)
-  #     ImageFile.get_path(dir, size, fname)
-  #   else
-  #     # return default image
-  #     ImageFile.get_path("/images/", size, "default-studio.png")
-  #   end
-  # end
+  def self.get_path(studio, size="medium")
+    if studio.photo?
+      return studio.photo(size)
+    end
+    # get path for image of size
+    # size should be either "thumb","small", or "medium"
+    dir = "/studiodata/" + studio.id.to_s() + "/profile/"
+    if studio.profile_image?
+      fname = File.basename(studio.profile_image)
+      ImageFile.get_path(dir, size, fname)
+    else
+      # return default image
+      ImageFile.get_path("/images/", size, "default-studio.png")
+    end
+  end
 
 end
