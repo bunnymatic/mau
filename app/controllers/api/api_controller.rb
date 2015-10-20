e3e75f133aec3c03c68fa70672634d3436595ce7
Mr Rogers <jon@bunnymatic.com>
2015-10-20 08:45:06 -0700

    add secret word auth

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -3,6 +3,10 @@ module Api
     before_filter :require_authorization
 
     def require_authorization
+      auth_key = request.headers['HTTP_AUTHORIZATION']
+      unless auth_key.present? && auth_key == Conf.api_consumer_key
+        render(text: "Unauthorized Request.  Access Denied.", status: :unauthorized) and return
+      end
     end
 
   end
