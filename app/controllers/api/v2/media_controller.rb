4184e71a1df1c7639bd6dbf96bc08d8514a3277c
Mr Rogers <jon@bunnymatic.com>
2015-10-19 19:36:13 -0700

    add media controller

---
+++ app/controllers/api/v2/media_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V2
+    class MediaController < Api::ApiController
+      respond_to :json, :xml
+      def index
+        respond_with Medium.all
+      end
+      def show
+        m = Medium.find(params[:id])
+        respond_with m
+      end
+    end
+  end
+end
