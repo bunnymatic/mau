c7d0aae7d57b057326fa27388d4130da65be7bc0
Mr Rogers <jon@bunnymatic.com>
2015-11-15 23:09:04 -0800

    allow internal asset api calls

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -4,10 +4,18 @@ module Api
 
     def require_authorization
       auth_key = request.headers['HTTP_AUTHORIZATION']
-      unless auth_key.present? && auth_key == Conf.api_consumer_key
+      unless !outside_request? || (auth_key.present? && auth_key == Conf.api_consumer_key)
        render(text: "Unauthorized Request.  Access Denied.", status: :unauthorized) and return
       end
     end
 
+    def outside_request?
+      begin
+        request.env["HTTP_REFERER"].nil? || (request.host != URI.parse(request.env["HTTP_REFERER"]).host)
+      rescue URI::Error => ex
+        true
+      end
+
+    end
   end
 end
