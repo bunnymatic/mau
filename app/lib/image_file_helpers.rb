6ba32fc836255201b45df0af51f28e4ff349a53c
Mr Rogers <jon@bunnymatic.com>
2015-11-23 08:09:21 -0800

    fix image file helpers

--- app/lib/image_file_helpers.rb
+++ app/lib/image_file_helpers.rb
@@ -5,22 +5,8 @@ module ImageFileHelpers
     fname.gsub(FILENAME_CLEANER,'').gsub(/\s+/, '')
   end
 
-  # def get_file_extension(fname)
-  #   ext = File::extname(fname).gsub /^\./, ''
-  #   if ext.empty?
-  #     raise MauImage::ImageError.new("Cannot determine file type without an extension.")
-  #   end
-  #   ext
-  # end
-
-  # def create_timestamped_filename(fname)
-  #   ts = Time.zone.now.to_i
-  #   clean_filename("#{ts}#{File::basename(fname)}")
-  # end
-
   def full_image_path(fname)
-    if /^http/ !~ fname
-      'http://' + Conf.site_url + fname
-    end
+    return fname if fname.starts_with?("http")
+    'http://' + Conf.site_url + fname
   end
 end
