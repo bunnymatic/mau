46f7ff8b4c202c25d6f96f866e56bbfdfcbebff2
Mr Rogers <jon@bunnymatic.com>
2015-12-31 15:12:20 -0800

    move find_random to art_piece

--- app/models/art_piece.rb
+++ app/models/art_piece.rb
@@ -95,8 +95,34 @@ class ArtPiece < ActiveRecord::Base
   end
 
 
+  def self.find_random(num = 1)
+    # skip out if we don't have any
+    return nil if (max = self.count) == 0
+
+    # don't request more than we have
+    num = [max,num].min
+
+    # build up a set of random offsets to go find
+    find_ids = [] # this is here for scoping
+
+    # get rid of the trivial cases
+    if 1 == num # we only want one - pick one at random
+      find_ids = [rand(max)]
+    else
+      # just randomise the set of possible ids
+      find_ids = (0..max-1).to_a.sort_by { rand }
+      # then grab out the number that we need
+      find_ids = find_ids.slice(0..num-1) if num != max
+    end
+
+    # we've got a random set of ids - now go pull out the records
+    ActiveRecord::Base.transaction do
+      find_ids.map {|the_id| owned.offset(the_id).take}
+    end
+  end
+
   def self.owned
-    where("artist_id in (select id from users where state = 'active' and type='Artist')")
+    joins(:artist).where( users: { state: 'active' } )
   end
 
   def tags
