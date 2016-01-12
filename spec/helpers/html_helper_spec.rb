8009845e54f0063dc1b38797f0100e9e2e83c572
Mr Rogers <jon@bunnymatic.com>
2016-01-12 07:47:58 -0800

    more moves to Capybara::Node::Simple

--- spec/helpers/html_helper_spec.rb
+++ spec/helpers/html_helper_spec.rb
@@ -12,20 +12,20 @@ describe HtmlHelper do
   describe '.print_html_list' do
     let(:links) { %w( one two three ) }
     let(:list) { helper.print_html_list('mylist', links).to_s }
-    let(:html_list) { HTML::Document.new(list).root }
+    let(:html_list) { Capybara::Node::Simple.new(list) }
     it 'lists all the links as a list' do
       links.each do |link|
         expect(list).to include link
       end
     end
     it 'includes a first/last class on the first/last element' do
-      assert_select html_list, 'li' do |tags|
-        expect(tags.first['class']).to include 'first'
-        expect(tags.last['class']).to include 'last'
-      end
+      expect(html_list).to have_css('li')
+      tags = html_list.all('li')
+      expect(tags.first['class']).to include 'first'
+      expect(tags.last['class']).to include 'last'
     end
     it 'includes a the input class on all elements' do
-      assert_select html_list, 'li.mylist', :count => links.length
+      expect(html_list).to have_css 'li.mylist', :count => links.length
     end
 
   end
