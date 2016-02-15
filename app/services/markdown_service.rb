9cf387f8e3b389332c14184ac4e854a2107766cb
Mr Rogers <jon@bunnymatic.com>
2016-02-15 15:25:51 -0800

    use rdiscount features for easier markdown cleanup

--- app/services/markdown_service.rb
+++ app/services/markdown_service.rb
@@ -1,11 +1,9 @@
 class MarkdownService
-  def self.cleanse(str)
-    doc = Nokogiri::HTML::DocumentFragment.parse(str)
-    doc.xpath(".//script").remove
-    doc.to_s
-  end
 
-  def self.markdown(discount, markdown_opts = :smart)
-    cleanse(RDiscount.new(discount || '', markdown_opts).to_html).html_safe
+  DEFAULT_MARKDOWN_OPTS = [:smart, :filter_html, :safelink]
+
+  def self.markdown(discount, markdown_opts = nil)
+    markdown_opts ||= DEFAULT_MARKDOWN_OPTS
+    RDiscount.new((discount || '').lstrip, *markdown_opts).to_html.html_safe
   end
 end
