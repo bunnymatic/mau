55282359296468871232e961aec1974a52e63066
Mr Rogers <jon@bunnymatic.com>
2016-02-21 17:20:26 -0800

    add to puma config

--- config/puma.rb
+++ config/puma.rb
@@ -7,6 +7,13 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+cwd = File.dirname(__FILE__) + "/.."
+
+directory cwd
+
+pidfile "#{cwd}/tmp/pids/puma.pid"
+state_path "#{cwd}/tmp/pids/puma.state"
+
 on_worker_boot do
   ActiveRecord::Base.establish_connection
 end
