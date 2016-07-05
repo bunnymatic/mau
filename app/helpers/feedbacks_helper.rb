e919945b5e820e29f2b939e497715475cb673814
Mr Rogers <jon@bunnymatic.com>
2016-07-05 08:46:04 -0700

    dump unused method in feedbacks helper

--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -4,14 +4,4 @@ module FeedbacksHelper
     link_to text, '#', :class => (["feedback_link", options[:class]].compact.join ' ')
   end
 
-  def feedback_skillsets
-    [ 'None',
-      'Web Design/Programming',
-      'Graphic Design',
-      'Marketing/PR',
-      'Manual Labor',
-      'Organizing People',
-      'Other' ]
-  end
-
 end
