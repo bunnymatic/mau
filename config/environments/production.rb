5af137eefda6dc8f94af1665a011a1acdb6653db
Mr Rogers <jon@bunnymatic.com>
2016-07-09 14:44:18 -0700

    don't mangle js so angular is happier'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,11 @@ Rails.application.configure do
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(
+    # for angular
+    mangle: false
+  )
+
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
