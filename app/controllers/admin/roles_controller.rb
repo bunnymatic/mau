afdd105bc38b6570857fe8d1b750a97d21571186
Mr Rogers <jon@bunnymatic.com>
2016-07-14 08:44:42 -0700

    clean up roles_controller

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -1,6 +1,8 @@
 module Admin
   class RolesController < ::BaseAdminController
     before_filter :admin_required
+    before_filter :load_role, only: [:show, :edit, :update, :destroy]
+
     def index
       @roles = Role.all
       @users_by_role = @roles.inject({}) do |memo, role|
@@ -31,38 +33,32 @@ module Admin
     end
 
     def show_or_edit
-      @role = Role.find(params[:id])
       @role_users = @role.users.active
       @users = (User.active.all - @role_users).sort_by{|u| u.full_name.downcase}
       render :edit
     end
 
     def update
-      if params[:id] && params[:user]
-        u = User.find(params[:user])
-        r = Role.find(params[:id])
-        begin
-          u.roles << r
-          u.save
-          flash[:notice] = "Added #{u.full_name} to role #{r.role}"
-        rescue ActiveRecord::RecordInvalid
-          flash[:notice] = "Looks like #{u.full_name} is already in that role."
-        end
+      u = User.find(params[:user])
+      begin
+        u.roles << @role
+        flash[:notice] = "Added #{u.full_name} to role #{@role.role}"
+      rescue ActiveRecord::RecordInvalid
+        flash[:notice] = "Looks like #{u.full_name} is already in that role."
       end
-      redirect_to admin_role_path(params[:id])
+      redirect_to admin_role_path(@role)
     end
 
     def destroy
-      r = Role.find(params[:id])
-      if r
-        name = r.role
-        r.destroy
-        flash[:notice] = "Removed #{name} role"
-      end
+      @role.destroy
+      flash[:notice] = "Removed #{@role.role} role"
       redirect_to admin_roles_path
     end
 
     private
+    def load_role
+      @role = Role.find(params[:id])
+    end
 
     def role_params
       params.require(:role).permit(:role)
