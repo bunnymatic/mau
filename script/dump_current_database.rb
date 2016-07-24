de4466fd3c8df42fcb780e5edf2a70f8e4ec5d61
Mr Rogers <jon@bunnymatic.com>
2016-07-24 16:04:22 -0700

    add script to dump db

---
+++ script/dump_current_database.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+MYSQL_DUMP_EXEC = 'mysqldump'
+
+def get_db_config
+  @dbconfig ||= YAML.load_file( Rails.root.join('config','database.yml')).fetch(Rails.env)
+end
+
+def db_name
+  get_db_config.fetch('database')
+end
+
+def get_db_cmdline_args
+  dbcnf = get_db_config
+  # NOTE: Assuming that database is running on localhost
+  # TODO - if you use other args like :socket, or ? they are ignored
+  # we could add host, port etc to make this more flexible
+  db_args = [['--user=','username'], ['--password=','password']].map do |entry|
+    if dbcnf[entry[1]].present?
+      "#{entry[0]}#{dbcnf[entry[1]]}"
+    end
+  end.compact
+  db_args += [ "--single-transaction"]
+end
+
+def destination_file
+  "mau_latest_#{Rails.env}.mysql"
+end
+
+cmd_args = [ MYSQL_DUMP_EXEC ] + get_db_cmdline_args + [ db_name, " > #{destination_file}" ]
+cmd = cmd_args.join(" ")
+print "Dumping #{db_name} to #{destination_file}..."
+`#{cmd}`
+puts "done"
