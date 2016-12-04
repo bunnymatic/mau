56660db6ba33dc7bfac906406fbbc906079a9b73
Mr Rogers <jon@carbonfive.com>
2016-12-03 16:54:37 -0800

    graph cleanup

--- app/assets/javascripts/admin/admin_graphs.js
+++ app/assets/javascripts/admin/admin_graphs.js
@@ -4,62 +4,50 @@
 jQuery(function() {
 
   if (jQuery('body.admin .graph').length) {
-    // graphs are on this page
-    var date_formatter = function(time_sec) {
-      var t = time_sec * 1000;
-      var dt = new Date(t);
-      return dt.getMonth() + "/" + dt.getDate() + "/" + (''+dt.getFullYear()).substr(2);
+    function dateFormatter(ts) {
+      var date = new Date(0);
+      var momentDate = moment(date.setSeconds(ts));
+      return momentDate.format('YYYY-MM-DD');
     };
 
-    var xticks = 5;
-    var yticks = 4;
-    var apd_options = {
-      xaxis: {
-        tickFormatter: date_formatter,
-        noTicks: xticks, max: new Date().valueOf()/1000
-      },
-      yaxis: {
-        noTicks: yticks,
-        min: 0
-      }
-    };
+    var DEFAULT_COLOR = '#c39f06';
 
     var GraphPerDay = {
-      load: function(selector, dataurl) {
+      load: function(selector, dataurl, seriesName) {
+        seriesName = seriesName || 'data';
         jQuery.ajax({
           url:dataurl,
           method: 'get',
           success: function(data, status, xhr) {
-            if (data) {
+            if (data && data.length) {
               var timestamp = data.map(function(entry) { return entry[0]; });
               var values = data.map(function(entry) { return entry[1]; });
               var columns = [
                 ['x'].concat(timestamp),
-                ['data'].concat(values)
+                [seriesName].concat(values)
               ];
+              colors = {}
+              colors[seriesName] = DEFAULT_COLOR;
               var chart = c3.generate({
                 bindto: selector,
                 data: {
                   x: 'x',
                   type: 'bar',
                   columns: columns,
-                  colors: {
-                    'data': '#d7682b'
-                  }
+                  colors: colors
                 },
                 axis: {
                   y: {
                     label: {
                       position: 'outer-center',
+                    },
+                    tick: {
+                      format: d3.format("d")
                     }
                   },
                   x: {
                     tick: {
-                      format: function(ts) {
-                        var date = new Date(0);
-                        var momentDate = moment(date.setSeconds(ts));
-                        return momentDate.format('YYYY-MM-DD');
-                      },
+                      format: dateFormatter,
                       rotate: 90,
                       centered: true,
                       fit: true
@@ -87,7 +75,7 @@ jQuery(function() {
           url: dataurl,
           method: 'get',
           success: function( data, status, xhr) {
-            if (data) {
+            if (data && data.length) {
               var chart = c3.generate({
                 bindto: selector,
                 data: {
@@ -96,7 +84,7 @@ jQuery(function() {
                   ],
                   type: 'bar',
                   colors: {
-                    '# pieces': '#d7682b'
+                    '# pieces': DEFAULT_COLOR
                   }
                 },
                 axis: {
@@ -125,11 +113,11 @@ jQuery(function() {
       }
     }
 
-    GraphPerDay.load('#artists_per_day', '/admin/stats/artists_per_day');
-    GraphPerDay.load('#user_visits_per_day', '/admin/stats/user_visits_per_day');
-    GraphPerDay.load('#favorites_per_day', '/admin/stats/favorites_per_day');
-    GraphPerDay.load('#art_pieces_per_day', '/admin/stats/art_pieces_per_day');
-    GraphPerDay.load('#os_signups', '/admin/stats/os_signups');
+    GraphPerDay.load('#artists_per_day', '/admin/stats/artists_per_day', 'artists');
+    GraphPerDay.load('#user_visits_per_day', '/admin/stats/user_visits_per_day', 'visits');
+    GraphPerDay.load('#favorites_per_day', '/admin/stats/favorites_per_day', 'favorites');
+    GraphPerDay.load('#art_pieces_per_day', '/admin/stats/art_pieces_per_day', 'pieces');
+    GraphPerDay.load('#os_signups', '/admin/stats/os_signups', 'sign ups');
     PlainGraph.load('#art_pieces_histogram', '/admin/stats/art_pieces_count_histogram');
 
   } // end if we're on the admin page with graphs
