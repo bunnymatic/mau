7b0e663c9dacb4ced922665926fa60f25930ad38
Mr Rogers <jon@bunnymatic.com>
2017-02-18 19:49:59 -0800

    Rubocop cleanup: artist_spec

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,7 +1,7 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 describe Artist do
-
   let(:max_pieces) { 10 }
   subject(:artist) { FactoryGirl.create(:artist, :active, :with_studio, :with_art, max_pieces: max_pieces, firstname: 'Joe', lastname: 'Blow') }
   let!(:open_studios_event) { FactoryGirl.create(:open_studios_event) }
@@ -32,34 +32,33 @@ describe Artist do
   end
   context 'make sure our factories work' do
     it 'creates an artist info with each artist' do
-      expect {
-        a = FactoryGirl.create(:artist, :active)
-      }.to change(ArtistInfo, :count).by 1
+      expect do
+        FactoryGirl.create(:artist, :active)
+      end.to change(ArtistInfo, :count).by 1
     end
   end
 
-  describe "create" do
+  describe 'create' do
     describe 'auth helpers' do
-      describe "make activation token " do
+      describe 'make activation token ' do
         let(:new_artist) { FactoryGirl.create(:artist) }
         let(:token) { new_artist.make_activation_code }
-        it "returns a string greater than 20 chars" do
+        it 'returns a string greater than 20 chars' do
           expect(token.length).to be > 20
         end
-        it "returns a string with only numbers and letters" do
-          expect(token).not_to match /\W+/
+        it 'returns a string with only numbers and letters' do
+          expect(token).not_to match(/\W+/)
         end
-        it "when called again returns something different" do
+        it 'when called again returns something different' do
           expect(token).not_to eql(new_artist.make_activation_code)
         end
       end
     end
-
   end
 
-  describe "update" do
-    it "should update bio" do
-      allow_any_instance_of(ArtistInfo).to receive(:compute_geocode).and_return([40,120])
+  describe 'update' do
+    it 'should update bio' do
+      allow_any_instance_of(ArtistInfo).to receive(:compute_geocode).and_return([40, 120])
 
       artist.bio = 'stuff'
       artist.artist_info.save!
@@ -67,8 +66,8 @@ describe Artist do
       expect(a.bio).to eql 'stuff'
     end
 
-    it "should update email settings" do
-      attr_hash = JSON::parse(artist.email_attrs)
+    it 'should update email settings' do
+      attr_hash = JSON.parse(artist.email_attrs)
       expect(attr_hash['fromartist']).to eql(true)
       expect(attr_hash['fromall']).to eql(true)
       attr_hash['favorites'] = false
@@ -87,12 +86,12 @@ describe Artist do
       @address_methods = [:address, :full_address]
     end
     describe 'artist info only' do
-      it "delegates address to artist info" do
+      it 'delegates address to artist info' do
         expect(artist_without_studio.artist_info.address).to eq artist_without_studio.address
       end
     end
     describe 'studio + artist info' do
-      it "returns studio address" do
+      it 'returns studio address' do
         @address_methods.each do |method|
           expect(artist.send(method)).not_to be_nil
           expect(artist.send(method)).to eq artist.studio.send(method)
@@ -100,81 +99,81 @@ describe Artist do
       end
     end
     describe 'neither address in artist info nor studio' do
-      it "returns empty for address" do
+      it 'returns empty for address' do
         expect(nobody.send(:address)).to be_empty
       end
       it { expect(nobody).to_not be_has_address }
     end
   end
   describe 'in_the_mission?' do
-    context "for artist in the mission with no studio" do
+    context 'for artist in the mission with no studio' do
       before do
         allow(artist_without_studio.artist_info).to receive(:lat).and_return(37.75)
         allow(artist_without_studio.artist_info).to receive(:lng).and_return(-122.41)
       end
-      it "returns true" do
+      it 'returns true' do
         expect(artist_without_studio).to have_address
         expect(artist_without_studio).to be_in_the_mission
       end
     end
-    context "for artist in the mission with a studio in the mission" do
+    context 'for artist in the mission with a studio in the mission' do
       before do
         allow(artist.artist_info).to receive(:lat).and_return(37.75)
         allow(artist.artist_info).to receive(:lng).and_return(-122.41)
         allow(artist.studio).to receive(:lat).and_return(37.75)
         allow(artist.studio).to receive(:lng).and_return(-122.41)
       end
-      it "returns true" do
+      it 'returns true' do
         expect(artist).to have_address
         expect(artist).to be_in_the_mission
       end
     end
-    it "returns false for artist with wayout address" do
+    it 'returns false for artist with wayout address' do
       expect(wayout_artist).to have_address
       expect(wayout_artist).to_not be_in_the_mission
     end
-    context "for artist not in the mission but in a studio in the mission" do
+    context 'for artist not in the mission but in a studio in the mission' do
       before do
         studio = FactoryGirl.create(:studio)
-        wayout_artist.update_attribute :studio, studio
+        wayout_artist.update_attributes studio: studio
         wayout_artist.reload
         allow(wayout_artist.studio).to receive(:lat).and_return(37.75)
         allow(wayout_artist.studio).to receive(:lng).and_return(-122.41)
       end
-      it "returns true" do
+      it 'returns true' do
         expect(wayout_artist).to have_address
         expect(wayout_artist).to be_in_the_mission
       end
     end
   end
   describe 'get from info' do
-    it "responds to method bio" do
+    it 'responds to method bio' do
       expect { artist.bio }.not_to raise_error
     end
   end
-  describe "fetch address" do
-    context "without studio association" do
+  describe 'fetch address' do
+    context 'without studio association' do
       let(:artist_info) { artist_without_studio.artist_info }
 
-      it "returns correct street" do
+      it 'returns correct street' do
         expect(artist_info.street).to eql artist.street
       end
-      it "returns correct address" do
+      it 'returns correct address' do
         expect(artist_without_studio.address.to_s).to include artist.street
       end
-      it "returns correct lat/lng" do
+      it 'returns correct lat/lng' do
         expect(artist_info.lat).to be
         expect(artist_info.lng).to be
       end
     end
     context 'with studio association' do
-      it "returns correct street" do
+      it 'returns correct street' do
         expect(artist.address.street).to eql studio.street
       end
-      it "returns correct address" do
+      it 'returns correct address' do
         expect(artist.address.to_s).to eql studio.address.to_s
       end
-      it "returns correct lat/lng" do
+      it 'returns correct lat/lng' do
         expect(artist.lat).to eql studio.lat
         expect(artist.lng).to eql studio.lng
       end
@@ -185,16 +184,16 @@ describe Artist do
       expect(artist.representative_piece).to eql artist.art_pieces[0]
     end
     it 'calls Cache.write if Cache.read returns nil' do
-      ap = ArtPiece.find_by_artist_id(artist.id)
+      ap = ArtPiece.find_by(artist_id: artist.id)
       allow(Rails.cache).to receive(:read).and_return(nil)
       expect(Rails.cache).to receive(:write).once
       allow(artist).to receive(:art_pieces).and_return([ap])
       expect(artist.representative_piece).to eql ap
     end
     it 'doesn\'t call Cache.write if Cache.read returns something' do
-      allow(Rails.cache).to receive(:read).
-                             with(artist.representative_art_cache_key).
-                             and_return(artist.art_pieces[0].id)
+      allow(Rails.cache).to receive(:read)
+        .with(artist.representative_art_cache_key)
+        .and_return(artist.art_pieces[0].id)
       expect(Rails.cache).to receive(:write).never
       artist.representative_piece
     end
@@ -221,17 +220,17 @@ describe Artist do
   describe 'destroying artists' do
     let(:quentin) { create(:artist, :with_art) }
     let(:art_piece) { quentin.art_pieces.first }
-    context "then artist removes that artpiece" do
+    context 'then artist removes that artpiece' do
       before do
-        create_favorite( artist, art_piece)
-        create_favorite( artist, quentin)
+        create_favorite(artist, art_piece)
+        create_favorite(artist, quentin)
 
         # validate fixtures setup
         expect(artist.favorites.map(&:favoritable_id)).to include art_piece.id
 
         art_piece.destroy
       end
-      it "art_piece is no longer in users favorite list" do
+      it 'art_piece is no longer in users favorite list' do
         expect(artist.reload.favorites.map(&:favoritable_id)).to_not include art_piece.id
       end
     end
@@ -242,24 +241,23 @@ describe Artist do
       artist
     end
     it 'tallies today\'s os participants' do
-      expect{ Artist.tally_os }.to change(OpenStudiosTally, :count).by(1)
+      expect { Artist.tally_os }.to change(OpenStudiosTally, :count).by(1)
     end
     it 'only records 1 entry per day' do
-      expect{
+      expect do
         2.times { Artist.tally_os }
-      }.to change(OpenStudiosTally, :count).by(1)
+      end.to change(OpenStudiosTally, :count).by(1)
     end
 
     it 'updates the record if it runs more than once 1 entry per day' do
       Artist.tally_os
-      a = Artist.all.reject{|a| a.doing_open_studios?}.first
+      a = Artist.all.reject(&:doing_open_studios?).first
       t = OpenStudiosTally.last
       a.artist_info.update_os_participation(open_studios_event.key, true)
       a.artist_info.save
       Artist.tally_os
       expect(OpenStudiosTally.last.count).to eql(t.count + 1)
     end
-
   end
 
   describe 'doing_open_studios?' do
@@ -279,17 +277,17 @@ describe Artist do
     before do
       artist
     end
-    it "most recent art piece should be the representative" do
+    it 'most recent art piece should be the representative' do
       expect(artist.representative_piece.title).to be_present
     end
 
-    it "returns art_pieces in by created at if there is no order" do
+    it 'returns art_pieces in by created at if there is no order' do
       expect(artist.art_pieces.to_a).to eql artist.art_pieces.sort_by(&:created_at).reverse
     end
 
-    it "returns art_pieces in by created at, then order if there is order" do
+    it 'returns art_pieces in by created at, then order if there is order' do
       artist.art_pieces.reverse.each_with_index do |ap, idx|
-        ap.update_attribute :position, idx
+        ap.update_attributes(position: idx)
       end
       expect(artist.art_pieces.map(&:position)).to be_strictly_decreasing
     end
@@ -300,11 +298,11 @@ describe Artist do
       artist
       wayout_artist
     end
-    describe ".without_art" do
+    describe '.without_art' do
       it 'does not include fans' do
         artists = Artist.without_art
         expect(artists.map(&:type).uniq).to eql ['Artist']
-        expect(artists.map{|a| a.art_pieces.count}.uniq).to eql [0]
+        expect(artists.map { |a| a.art_pieces.count }.uniq).to eql [0]
       end
     end
     describe '.with_representative_image' do
@@ -312,8 +310,8 @@ describe Artist do
         active = Artist.active
         w_image = Artist.active.with_representative_image.all
         expect(active.count).not_to eql w_image.count
-        expect(active.select{|a| a.representative_piece.blank?}.size).to be >= 1
-        expect(w_image.select{|a| a.representative_piece.blank?}).to be_empty
+        expect(active.select { |a| a.representative_piece.blank? }.size).to be >= 1
+        expect(w_image.select { |a| a.representative_piece.blank? }).to be_empty
       end
     end
     describe '.open_studios_participants' do
@@ -324,7 +322,7 @@ describe Artist do
       it 'returns 0 artists for an unknown os' do
         expect(Artist.open_studios_participants('200810').count).to eql 0
       end
-      it "returns 1 artist(s) for the current open studios" do
+      it 'returns 1 artist(s) for the current open studios' do
         artists = Artist.open_studios_participants
         expect(artists.size).to eq(1)
       end
