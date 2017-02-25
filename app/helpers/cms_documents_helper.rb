76d2601f7c311d710f4c229b0c7bbcbf761a362e
Mr Rogers <jon@carbonfive.com>
2017-02-25 14:49:59 -0800

    Missed one is_editor

--- app/helpers/cms_documents_helper.rb
+++ app/helpers/cms_documents_helper.rb
@@ -4,7 +4,7 @@ module CmsDocumentsHelper
     cms_content ||= {}
     data = Hash[cms_content.except(:content).map { |(key, val)| ['data-%s' % key, val] }]
     clz = %( section markdown )
-    clz << 'editable' if current_user.try(:is_editor?)
+    clz << 'editable' if current_user.try(:editor?)
     content_tag(:div, data.merge(class: clz)) do
       concat cms_content[:content]
       yield if block_given?
