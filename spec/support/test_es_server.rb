bd2e292e4ab243ad627736c17b21f6ab8035ae12
Mr Rogers <jon@carbonfive.com>
2017-09-10 13:10:26 -0700

    Better running test for test es server

--- spec/support/test_es_server.rb
+++ spec/support/test_es_server.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 require 'elasticsearch/extensions/test/cluster'
 class TestEsServer
+
+  DEFAULT_TEST_ARGS = { number_of_nodes: 1, clear_cluster: true }
+
   def self.cluster
     Elasticsearch::Extensions::Test::Cluster
   end
@@ -9,15 +12,18 @@ class TestEsServer
     @port ||= URI.parse(Rails.application.config.elasticsearch_url).port
   end
 
+  def self.server_args
+    DEFAULT_TEST_ARGS.merge(port: port)
+  end
+
   def self.running?
-    cluster.running?(port: port)
+    cluster.running?(server_args)
   end
 
   def self.start
     return if running?
-
     puts "Starting elasticsearch cluster on port #{port}"
-    cluster.start(port: port, number_of_nodes: 1, clear_cluster: true) unless running?
+    cluster.start(server_args) unless running?
   end
 
   def self.stop
