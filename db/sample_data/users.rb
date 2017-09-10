987625b250bbc52be5e30c20e3497e6181322e42
Mr Rogers <jon@carbonfive.com>
2017-09-10 08:44:41 -0700

    Better sample data names/logins/emails

--- db/sample_data/users.rb
+++ db/sample_data/users.rb
@@ -2,14 +2,14 @@ def ensure_user(login, *factory_args)
 
   unless User.find_by(login: login)
     traits, opts = factory_args.partition { |arg| !arg.is_a? Hash }
-    options = opts.reduce Hash.new, :merge
-    FactoryGirl.create(*traits, options)
-    puts "--> Created account for #{login}"
+    options = (opts.reduce Hash.new, :merge).merge(login: login, email: "#{login}@example.com")
+    user = FactoryGirl.create(*traits, options)
+    puts "--> Created account for #{user.login} <#{user.email}>"
   end
 end
 
 ensure_user("artist", :artist, :active, :with_art)
 ensure_user("pending", :artist, :pending)
 ensure_user("suspended", :artist, state: :suspended)
-ensure_user("admin", :artist, :admin, :with_art)
+ensure_user("administrator", :artist, :admin, :with_art)
 ensure_user("artfan", :fan)
