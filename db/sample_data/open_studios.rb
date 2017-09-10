aefb511848840257c209c7578468b2069d33ec29
Mr Rogers <jon@carbonfive.com>
2017-09-10 13:10:26 -0700

    rubocop

--- db/sample_data/open_studios.rb
+++ db/sample_data/open_studios.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 def ensure_open_studios_event(start_date)
-  OpenStudiosEvent.find_or_create_by(key: start_date.strftime("%Y%m")) do |os|
+  OpenStudiosEvent.find_or_create_by(key: start_date.strftime('%Y%m')) do |os|
     os.assign_attributes(FactoryGirl.attributes_for(:open_studios_event, start_date: start_date))
   end
   puts "--> Added Open Studios Event for #{start_date}"
--- db/sample_data/studios.rb
+++ db/sample_data/studios.rb
@@ -1,17 +1,17 @@
+# frozen_string_literal: true
 def ensure_studio(name, *factory_args)
-
   unless User.find_by(name: name)
     traits, opts = factory_args.partition { |arg| !arg.is_a? Hash }
-    options = opts.reduce Hash.new, :merge
+    options = opts.reduce({}, :merge)
     FactoryGirl.create(*traits, options)
     puts "--> Created studio #{name}"
   end
 end
 
-ensure_studio("1890 Bryant", :studio)
-ensure_studio("Workspace", :studio)
-ensure_studio("Over There", :studio)
+ensure_studio('1890 Bryant', :studio)
+ensure_studio('Workspace', :studio)
+ensure_studio('Over There', :studio)
 
 Studio.all.each do |studio|
-  studio.artists << Artist.active.order("rand()").limit(1)
+  studio.artists << Artist.active.order('rand()').limit(1)
 end
