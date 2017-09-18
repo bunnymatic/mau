f62117183f653558b938de9a557e4fc5e1b226c9
Mr Rogers <jon@carbonfive.com>
2017-09-18 07:53:13 -0700

    Rubocop

--- app/presenters/open_studios_event_presenter.rb
+++ app/presenters/open_studios_event_presenter.rb
@@ -15,21 +15,21 @@ class OpenStudiosEventPresenter < ViewPresenter
   end
 
   def link_text
-    date_range + " " + model.start_date.strftime("%Y")
+    date_range + ' ' + model.start_date.strftime('%Y')
   end
 
   def date_range
     same_month = model.start_date.month == model.end_date.month
     same_day = model.start_date.day == model.end_date.day
-    return model.start_date.strftime("%b %d") if same_day
+    return model.start_date.strftime('%b %d') if same_day
 
     if same_month
       [
-        model.start_date.strftime("%b"),
-        model.start_date.strftime("%d") + "-" + model.end_date.strftime("%d")
-      ].join(" ")
+        model.start_date.strftime('%b'),
+        model.start_date.strftime('%d') + '-' + model.end_date.strftime('%d')
+      ].join(' ')
     else
-      model.start_date.strftime("%b %d") + "-" + model.end_date.strftime("%b %d")
+      model.start_date.strftime('%b %d') + '-' + model.end_date.strftime('%b %d')
     end
   end
 
