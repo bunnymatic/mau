7fb3a56cc104f20ac017fd557bfa37ed79a7b42d
Mr Rogers <jon@carbonfive.com>
2017-10-31 19:21:29 -0700

    Randomize cucumber and fix script for selectize

--- features/support/selectize_select.rb
+++ features/support/selectize_select.rb
@@ -8,8 +8,9 @@ module SelectizeSelect
   # Select a single item from a selectized select input where multiple=false given the id for base field
   def selectize_single_select(key, value)
     # It may be tempting to combine these into one execute_script, but don't; it will cause failures.
-    page.execute_script %{ #{find_selectized_control_js(key)}.find('.selectize-input').click(); }
-    page.execute_script %{ #{find_selectized_control_js(key)}.find('.selectize-dropdown-content .option:contains("#{value}")').click(); }
+"#{value}")').click(); }
+    page.execute_script %{ #{find_selectized_control_js(key).strip}.find('.selectize-input').click(); }
+    page.execute_script %{ #{find_selectized_control_js(key).strip}.find('.selectize-dropdown-content .option:contains("#{value}")').click(); }
   end
 
   # Select one or more items from a selectized select input where multiple=true.
