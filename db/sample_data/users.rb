3efcf9db6d909d5000ff8646dff4bb8cf7aad1df
Mr Rogers <jon@carbonfive.com>
2017-11-04 11:33:59 -0700

    Add favorites to sample data

--- db/sample_data/users.rb
+++ db/sample_data/users.rb
@@ -1,16 +1,24 @@
 # frozen_string_literal: true
 def ensure_user(login, *factory_args)
-  return if User.find_by(login: login)
+  user = User.find_by(login: login)
+  return user if User.find_by(login: login)
 
   traits, opts = factory_args.partition { |arg| !arg.is_a? Hash }
   options = opts.reduce({}, :merge).merge(login: login, email: "#{login}@example.com")
   user = FactoryBot.create(*traits, options)
   puts "--> Created account for #{user.login} <#{user.email}>"
+  user
 end
 
-ensure_user('artist', :artist, :active, :with_art)
-ensure_user('no_address', :artist, :active, :without_address)
+artist = ensure_user('artist', :artist, :active, :with_art)
+addressless = ensure_user('no_address', :artist, :active, :without_address)
 ensure_user('pending', :artist, :pending)
 ensure_user('suspended', :artist, state: :suspended)
-ensure_user('administrator', :artist, :admin, :with_art)
-ensure_user('artfan', :fan)
+admin = ensure_user('administrator', :artist, :admin, :with_art)
+fan = ensure_user('artfan', :fan)
+
+puts "--> Adding favorites"
+FavoritesService.add(fan, artist.art_pieces.first)
+FavoritesService.add(fan, artist)
+FavoritesService.add(addressless, artist)
+FavoritesService.add(artist, admin)
