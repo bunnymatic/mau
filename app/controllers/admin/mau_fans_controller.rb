0ef9d02e1efbdbff0dc8e3a6216c66a1446c17be
Mr Rogers <jon@carbonfive.com>
2017-11-04 11:24:15 -0700

    Add info to fans index page

--- app/controllers/admin/mau_fans_controller.rb
+++ app/controllers/admin/mau_fans_controller.rb
@@ -2,7 +2,7 @@
 module Admin
   class MauFansController < ::BaseAdminController
     def index
-      @fans = MauFan.active
+      @fans = MauFan.active.map { |fan| UserPresenter.new(fan) }
     end
   end
 end
