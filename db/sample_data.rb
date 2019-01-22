2945b6edb40b60109632720c7f551ee20cff7a81
Jon Rogers <jon@carbonfive.com>
2019-01-21 23:16:46 -0800

    add sample data so we have a pile of lastname starts with A artists"

--- db/sample_data.rb
+++ db/sample_data.rb
@@ -5,6 +5,8 @@ require 'faker'
 
 Dir[Rails.root.join('spec', 'factories', '**', '*.rb')].each { |f| require f }
 
+ActionMailer::Base.perform_deliveries = false
+
 require_relative 'sample_data/users'
 require_relative 'sample_data/studios'
 require_relative 'sample_data/open_studios'
