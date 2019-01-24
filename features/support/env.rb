407c6a4aef8dc3d7b795a4c5ed7e66173225b272
Jon Rogers <jon@carbonfive.com>
2019-01-23 22:49:08 -0800

    Harden test around suspending artist

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,6 +12,8 @@ require './spec/support/simplecov'
 require './spec/support/faker_files'
 require 'cucumber/rails'
 require 'factory_bot'
+require 'capybara/rspec'
+require 'capybara-screenshot/cucumber'
 
 Dir[Rails.root.join('spec', 'factories', '**', '*.rb')].each { |f| require f }
 Dir[Rails.root.join('features', 'support', '**', '*.rb')].each { |f| require f }
