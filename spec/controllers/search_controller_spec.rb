af77ddae60517e1f7de30166cef77bc1ad2a3b95
Jon Rogers <jon@carbonfive.com>
2019-01-24 11:37:53 -0800

    Five-O!

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -6,7 +6,7 @@ describe SearchController, elasticsearch: false do
   describe '#index' do
     context 'finding by studio' do
       before do
-        get :index, params: { q: "stuff" }
+        get :index, params: { q: 'stuff' }
       end
       it 'returns success' do
         expect(response).to be_successful
