ba8e08ef1e16d4e9a0c5432de469af2a08c2a7c4
Jon Rogers <jon@carbonfive.com>
2019-01-24 11:13:29 -0800

    Trim search controller spec to be as minimal as possible

--- spec/controllers/search_controller_spec.rb
+++ spec/controllers/search_controller_spec.rb
@@ -2,23 +2,11 @@
 
 require 'rails_helper'
 
-describe SearchController, elasticsearch: true do
-  let!(:studios) { FactoryBot.create_list :studio, 4 }
-  let!(:artists) do
-    FactoryBot.create_list(:artist, 2, :active, :with_art, firstname: 'name1', studio: studios_search.first) +
-      FactoryBot.create_list(:artist, 2, :active, :with_art, firstname: 'name1', studio: studios_search.last)
-  end
-  let(:media_search) { artists.map { |a| a.art_pieces.map(&:medium) }.flatten.compact[0..1] }
-  let(:studios_search) { studios[0..1] }
-
-  before do
-    stub_search_service!
-  end
-
+describe SearchController, elasticsearch: false do
   describe '#index' do
     context 'finding by studio' do
       before do
-        get :index, params: { q: studios.first.name.split.first }
+        get :index, params: { q: "stuff" }
       end
       it 'returns success' do
         expect(response).to be_successful
