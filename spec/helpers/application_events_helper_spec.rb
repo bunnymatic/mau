cbfb902caf86bdaab419cb5e1db3c3ea8106229c
Jon Rogers <jon@carbonfive.com>
2019-04-04 14:35:01 -0700

    Update app events to show all chanes in one line with details

--- spec/helpers/application_events_helper_spec.rb
+++ spec/helpers/application_events_helper_spec.rb
@@ -6,12 +6,12 @@ describe ApplicationEventsHelper do
   describe 'link_to_user' do
     let(:artist) { FactoryBot.create :artist, :active }
     let(:no_user_event) { FactoryBot.create :generic_event, message: 'blop', data: { 'user' => nil } }
-    let(:event) { FactoryBot.create :open_studios_signup_event, message: 'blop', data: { 'user' => artist.id } }
+    let(:event) { FactoryBot.create :open_studios_signup_event, message: 'blop', data: { 'user' => artist.login, 'user_id' => artist.id } }
 
-    it { expect(helper.link_to_user(no_user_event)).to eql '' }
+    it { expect(helper.link_to_event_user(no_user_event)).to eql '' }
     it 'links to the artist' do
-      link_text = helper.link_to_user(event)
-      expect(link_text).to eql(link_to(artist.id, artist_path(artist.id)))
+      link_text = helper.link_to_event_user(event)
+      expect(link_text).to eql(link_to(artist.login, artist_path(artist.id)))
     end
   end
 end
