4845923db5a7eb0a7dbcadae09b546da4f33d970
Jon Rogers <jon@carbonfive.com>
2020-03-01 18:00:31 -0800

    Handle the case when there are no open studios events
    Problem
    -------
    
    The `remind_for_open_studios_register?` method is counting on
    `OpenStudiosEventService.current` returning a thing.  But it's possible
    that there are no events to return (rare, but possible).
    
    Solution
    --------
    
    Return false to remind folks if there is no current open studios event.
    And... if there is one, do the rest of the logic.

--- spec/presenters/user_navigation_spec.rb
+++ spec/presenters/user_navigation_spec.rb
@@ -9,24 +9,49 @@ describe UserNavigation do
   describe 'remind_for_open_studios_register?' do
     let(:artist_to_remind) { create(:artist) }
     let(:artist_to_remind_nav) { described_class.new(:artist_to_remind) }
-    let(:future_open_studios_event) { FactoryBot.create(:open_studios_event, :future) }
 
     before do
       Timecop.freeze
-      allow(OpenStudiosEventService).to receive(:current).and_return(future_open_studios_event)
-      artist.open_studios_events << future_open_studios_event
     end
 
     after do
       Timecop.return
     end
 
-    it 'returns true if artist has not registered for current event and it is no more than 12 weeks away' do
-      expect(artist_to_remind_nav.remind_for_open_studios_register?).to eq(true)
+    context 'when there is a future open studios event' do
+      let(:future_open_studios_event) { FactoryBot.create(:open_studios_event, :future) }
+
+      before do
+        Timecop.freeze
+        artist.open_studios_events << future_open_studios_event
+      end
+
+      after do
+        Timecop.return
+      end
+
+      it 'returns true if artist has not registered for current event and it is no more than 12 weeks away' do
+        expect(artist_to_remind_nav.remind_for_open_studios_register?).to eq(true)
+      end
+
+      it 'returns false if the artist is already registered for current event' do
+        expect(artist_nav.remind_for_open_studios_register?).to eq(false)
+      end
     end
 
-    it 'returns false if the artist is already registered for current event' do
-      expect(artist_nav.remind_for_open_studios_register?).to eq(false)
+    context 'when there are no open studios events' do
+      it 'returns false' do
+        expect(artist_to_remind_nav.remind_for_open_studios_register?).to eq(false)
+      end
+    end
+
+    context 'when there is no future open studios event' do
+      before do
+        create(:open_studios_event, start_date: 1.month.ago)
+      end
+      it 'returns false' do
+        expect(artist_to_remind_nav.remind_for_open_studios_register?).to eq(false)
+      end
     end
   end
 end
