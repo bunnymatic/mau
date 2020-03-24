216847ce5c5177d930c777f1d8de858088445b1a
Jon Rogers <jon@carbonfive.com>
2020-03-23 18:45:10 -0700

    Repair missing timeago directive

---
+++ app/webpack/components/time_ago/time_ago.js
@@ -0,0 +1,18 @@
+import angular from "angular";
+import ngInject from "@js/ng-inject";
+
+const timeAgo = ngInject(function (moment) {
+  return {
+    restrict: "E",
+    scope: {
+      time: "@",
+    },
+    template:
+      "<span class='time-ago' title={{pacificTime}}>{{formattedTime}}</span>",
+    link: function ($scope, _el, _attrs) {
+      $scope.pacificTime = moment($scope.time).tz("America/Los_Angeles");
+      $scope.formattedTime = moment($scope.time).fromNow();
+    },
+  };
+});
+angular.module("mau.directives").directive("timeAgo", timeAgo);
