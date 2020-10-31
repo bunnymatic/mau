c843bd47f121a2890d87a76a4b3e33cd99d6b2da
Jon Rogers <jon@carbonfive.com>
2020-10-31 14:22:16 -0700

    fix up os key gen test

--- app/webpack/js/admin/os_key_generator.test.js
+++ app/webpack/js/admin/os_key_generator.test.js
@@ -14,20 +14,17 @@ describe("OsKeyGenerator", function () {
     document.documentElement.innerHTML =
       '<div id="fixture">' +
       " <form class='js-os-key-gen'>" +
-      "    <input id='start_date'></input>" +
-      "    <input class='js-datepicker' id='start_date_pickadate_hidden'></input>" +
+      "    <input class='js-datepicker' id='start_date'></input>" +
       "    <input id='end_date'></input>" +
       "  <input id='os_key'></input>" +
       " </form>" +
       "</div>";
     jQuery("#fixture input").val("");
-    jQuery(".js-datepicker").pickadate({
-      min: new Date(),
-    });
+    jQuery(".js-datepicker").pickadate({});
   });
   describe("initialization", function () {
     it("sets the key if the start date is set", function () {
-      jQuery("#start_date_pickadate_hidden").val("2012-01-10");
+      jQuery("#start_date").pickadate("picker").set("select", [2012, 0, 10]);
       new OsKeyGenerator(jQuery("form.js-os-key-gen"), {
         start_date_field: "#start_date",
         key_field: "#os_key",
@@ -35,14 +32,6 @@ describe("OsKeyGenerator", function () {
       expect(jQuery("#fixture #os_key").val()).toEqual("201201");
     });
     it("does not set the key if start date is empty", function () {
-      new OsKeyGenerator(jQuery("form.js-os-key-gen"), {
-        start_date_field: "#start_date",
-        key_field: "#os_key",
-      });
-      expect(jQuery("#fixture #os_key").val()).toEqual("");
-    });
-    it("does not set the key if start date is an invalid date", function () {
-      jQuery("#start_date_pickadate_hidden").val("2012-xx-10");
       new OsKeyGenerator(jQuery("form.js-os-key-gen"), {
         start_date_field: "#start_date",
         key_field: "#os_key",
