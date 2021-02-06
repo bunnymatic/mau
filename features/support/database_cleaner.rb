7f1a748c6d41597ea7419ff06c083a73f4eb99a2
Jon Rogers <jon@carbonfive.com>
2021-02-06 14:20:01 -0800

    Upgrade cucumber/cucumber-rails
    Upgrade cucumber and cucumber rails to the new versions.
    
    And while we're here, bump `database_cleaner` and update requires around
    that.
    
    Note:
    
    There was an issue around this where the flip between :transaction and
    :truncation database strategies was causing issues. So we've moved the
    entire cucumber suite to use :truncation for safety.  You can read more
    here (if you want): https://github.com/cucumber/cucumber-rails/issues/490

--- features/support/database_cleaner.rb
+++ features/support/database_cleaner.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 begin
-  require 'database_cleaner'
+  require 'database_cleaner/active_record'
   require 'database_cleaner/cucumber'
 
+  # This app seems to have some trouble with `:transaction` and flipping to `:truncation` only
+  # for JS tests so, let's just leave it alone.
+  # For more details: https://github.com/cucumber/cucumber-rails/issues/490
   DatabaseCleaner.strategy = :truncation
 rescue NameError
   raise 'You need to add database_cleaner to your Gemfile (in the :test group) if you wish to use it.'
--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'database_cleaner/active_record'
+
 RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:deletion)
