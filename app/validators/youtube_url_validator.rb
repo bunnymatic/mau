7e62693a9aff7e91d62367a3fa30a7789bd3e921
Jon Rogers <jon@carbonfive.com>
2021-02-28 12:40:19 -0800

    lint

--- app/validators/youtube_url_validator.rb
+++ app/validators/youtube_url_validator.rb
@@ -1,27 +1,27 @@
 class YoutubeUrlValidator < ActiveModel::EachValidator
-  YOUTUBE_URL_REGEX = %r{^((?:https?:)?//)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(/(?:[\w\-]+\?v=|embed/|v/)?)([\w\-]+)(\S+)?$}
+  YOUTUBE_URL_REGEX = %r{^((?:https?:)?//)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(/(?:[\w\-]+\?v=|embed/|v/)?)([\w\-]+)(\S+)?$}.freeze
 
   def validate_each(record, attribute, value)
     # test for presence separately
     return true if value.blank?
 
-    unless is_a_valid_url(value)
+    unless valid_url?(value)
       record.errors.add(attribute,
                         options[:message] || 'is not a valid URL. Did you remember including http:// or https:// at the beginning?')
       return
     end
 
-    unless YOUTUBE_URL_REGEX.match?(value)
-      record.errors.add(attribute,
-                        options[:message] || 'does not look like a Youtube link.')
-    end
+    return if YOUTUBE_URL_REGEX.match?(value)
+
+    record.errors.add(attribute,
+                      options[:message] || 'does not look like a Youtube link.')
   end
 
   private
 
-  def is_a_valid_url(value)
+  def valid_url?(value)
     URI.parse(value).is_a?(URI::HTTP)
-  rescue URI::InvalidURIError => e
+  rescue URI::InvalidURIError
     false
   end
 end
