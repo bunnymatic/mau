784be10ea4f8ed635508199dbf9f1267e65da65a
Jon Rogers <jon@carbonfive.com>
2021-03-07 22:25:20 -0800

    Take timezone out of the picture

--- app/webpack/js/app/time_utils.test.js
+++ app/webpack/js/app/time_utils.test.js
@@ -4,13 +4,10 @@ import { parseTimeSlot } from "./time_utils";
 
 describe("time_utils", () => {
   describe("parseTimeSlot", () => {
-    beforeEach(() => {
-      DateTime.local().setZone("America/Los_Angeles");
-    });
     it("returns timeslots parsed into date times", () => {
       expect(parseTimeSlot("1605135600::1605139200")).toEqual({
-        start: DateTime.local(2020, 11, 11, 15, 0, 0),
-        end: DateTime.local(2020, 11, 11, 16, 0, 0),
+        start: DateTime.fromSeconds(1605135600),
+        end: DateTime.fromSeconds(1605139200),
       });
     });
   });
--- app/webpack/js/app/time_utils.ts
+++ app/webpack/js/app/time_utils.ts
@@ -1,10 +1,9 @@
 import { DateTime } from "luxon";
 import * as types from "@reactjs/types";
 
-const SECONDS_TO_MILLISECONDS = 1000;
 export const parseTimeSlot = (timeSlot: string): types.TimeSlot => {
   const [start, end] = timeSlot
     .split("::")
-    .map((v) => DateTime.fromMillis(v * SECONDS_TO_MILLISECONDS));
+    .map((v) => DateTime.fromSeconds(parseInt(v, 10)));
   return { start, end };
 };
