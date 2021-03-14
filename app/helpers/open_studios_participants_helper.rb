f42b04fd6eba75bab7d3c9e6b4ccc50333f5479c
Jon Rogers <jon@carbonfive.com>
2021-03-14 11:13:25 -0700

    Add os participant helper spec

--- app/helpers/open_studios_participants_helper.rb
+++ app/helpers/open_studios_participants_helper.rb
@@ -1,8 +1,11 @@
 module OpenStudiosParticipantsHelper
   def display_time_slot(timeslot)
     start_time, end_time = OpenStudiosParticipant.parse_time_slot(timeslot)
-    "#{start_time.to_s(:admin_date_only)}: " \
-      "#{start_time.in_time_zone(Conf.event_time_zone).to_s(:time_only)} - " +
-      end_time.in_time_zone(Conf.event_time_zone).to_s(:time_with_zone).to_s
+    [
+      start_time.to_s(:date_month_first),
+      start_time.in_time_zone(Conf.event_time_zone).to_s(:time_only),
+      ' -',
+      end_time.in_time_zone(Conf.event_time_zone).to_s(:time_with_zone),
+    ].join
   end
 end
---
+++ spec/helpers/open_studios_participants_helper_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe OpenStudiosParticipantsHelper do
+  def make_1hour_timeslot(t) # rubocop:disable Naming/MethodParameterName
+    [t, t + 1.hour].map(&:to_i).join('::')
+  end
+
+  test_scenarios = [
+    [Time.zone.local(2021, 5, 6, 1), '05/06/2021 1:00am - 2:00am PDT'],
+    [Time.zone.local(2021, 10, 12, 14), '10/12/2021 2:00pm - 3:00pm PDT'],
+  ]
+
+  test_scenarios.each do |scenario|
+    it "returns #{scenario.last} for the date #{scenario.first}" do
+      expect(helper.display_time_slot(make_1hour_timeslot(scenario.first))).to eq scenario.last
+    end
+  end
+end
