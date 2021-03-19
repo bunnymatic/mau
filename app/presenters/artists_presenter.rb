51c973f203346ba78f4433c03038810691c0a311
Jon Rogers <jon@carbonfive.com>
2021-03-18 22:19:54 -0700

    rubocop guard fixes

--- app/presenters/artists_presenter.rb
+++ app/presenters/artists_presenter.rb
@@ -9,14 +9,16 @@ class ArtistsPresenter < ViewPresenter
   end
 
   def artists
-    @artists ||=
-      begin
-        if os_only
-          OpenStudiosEvent.current.artists.includes(:open_studios_participants).active
-          # base_artists.in_the_mission.select(&:doing_open_studios?)
-        else
-          Artist.active.includes(:studio, :artist_info, :art_pieces, :open_studios_events)
-        end.sort_by(&:sortable_name)
-      end.map { |artist| ArtistPresenter.new(artist) }
+    return @artists if @artists
+    return Artist.none unless OpenStudiosEvent.current
+
+    @artists = begin
+      if os_only
+
+        OpenStudiosEvent.current.artists.includes(:open_studios_participants).active
+      else
+        Artist.active.includes(:studio, :artist_info, :art_pieces, :open_studios_events)
+      end.sort_by(&:sortable_name)
+    end.map { |artist| ArtistPresenter.new(artist) }
   end
 end
