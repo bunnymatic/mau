8ed9e016ec76a287b595fd8b55f51348778c81f8
Jon Rogers <jon@carbonfive.com>
2021-03-21 18:17:07 -0700

    Clean up after self PR review

--- app/webpack/reactjs/components/close_button.test.tsx
+++ app/webpack/reactjs/components/close_button.test.tsx
@@ -6,14 +6,17 @@ import { CloseButton } from "./close_button";
 describe("CloseButton", () => {
   let mockHandleClose = jest.fn();
   let container;
+
   beforeEach(() => {
     const rendered = render(<CloseButton handleClick={mockHandleClose} />);
     container = rendered.container;
     jest.resetAllMocks();
   });
+
   it("matches the snapshot", () => {
     expect(container).toMatchSnapshot();
   });
+
   it("runs handle close when you click close", () => {
     const button = screen.getByTitle("close");
     fireEvent.click(button);
