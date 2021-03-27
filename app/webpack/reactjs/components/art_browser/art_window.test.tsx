8e1bc7aea99b83895dfe8e4c6111733ccc8c8f98
Jon Rogers <jon@carbonfive.com>
2021-03-26 19:02:14 -0700

    ArtModal rename/refactor
    In an effort to unblock other stories, I tried to extract some bits
    from the ArtModal so that detail elements inside the now ArtWindow
    can be worked on independently.
    
    Changes
    -------
    
    * extract ArtWindow component which just has the picture and annotations.
    * extract it's styles

---
+++ app/webpack/reactjs/components/art_browser/art_window.test.tsx
@@ -0,0 +1,18 @@
+import { ArtPiece } from "@models/art_piece.model";
+import { jsonApiArtPieceFactory as artPieceFactory } from "@test/factories";
+import { render } from "@testing-library/react";
+import React from "react";
+
+import { ArtWindow } from "./art_window";
+
+describe("ArtWindow", () => {
+  let artPiece;
+  beforeEach(() => {
+    artPiece = new ArtPiece(artPieceFactory.build());
+    console.log(artPiece);
+  });
+  it("matches the snapshot", () => {
+    const { container } = render(<ArtWindow art={artPiece} />);
+    expect(container).toMatchSnapshot();
+  });
+});
