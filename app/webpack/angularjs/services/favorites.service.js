f6d39347ded1e0109c0dc1af372040eb0f12f2c1
Jon Rogers <jon@carbonfive.com>
2021-06-05 23:51:22 -0700

    remove more unused angular

--- app/webpack/angularjs/services/favorites.service.js
+++
@@ -1,8 +0,0 @@
-import * as svc from "@services/favorites.service";
-import angular from "angular";
-
-const favoritesService = () => {
-  return svc;
-};
-
-angular.module("mau.services").factory("favoritesService", favoritesService);
