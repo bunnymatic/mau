6d003e60e43665ed808a6bc44c2a5b880b8e74d6
Jon Rogers <jon@carbonfive.com>
2021-09-28 18:55:07 -0700

    Add new icon and scroll into view
    Problem
    --------
    
    We got a new QR code for venmo and it's visibility isn't great if you
    click the button and it opens down off the bottom of the screen.
    
    Solution
    ---------
    
    Add the new icon and scroll into view

--- app/webpack/reactjs/components/donate_button.tsx
+++ app/webpack/reactjs/components/donate_button.tsx
@@ -2,16 +2,23 @@ import { MauButton } from "@reactjs/components/mau_button";
 import { useModalState } from "@reactjs/hooks";
 import { getDonateLink } from "@services/venmo_donate_link.service";
 import cx from "classnames";
-import React, { FC } from "react";
+import React, { FC, useRef } from "react";
 
 import QrCode from "../../images/mau_venmo_qrcode.jpg";
 
 export const DonateButton: FC = () => {
   const { toggle, isOpen } = useModalState(false);
+  const qrCodeRef = useRef<HTMLElement>(null);
+  const toggleAndScrollIntoView = () => {
+    toggle();
+    if (!isOpen) {
+      qrCodeRef.current?.scrollIntoView();
+    }
+  };
 
   return (
     <>
-      <MauButton secondary onClick={toggle}>
+      <MauButton secondary onClick={toggleAndScrollIntoView}>
         Donate
       </MauButton>
       <div
@@ -19,7 +26,7 @@ export const DonateButton: FC = () => {
           "donate-button__qrcode--open": isOpen,
         })}
       >
-        <a href={getDonateLink()} target="_blank">
+        <a href={getDonateLink()} target="_blank" ref={qrCodeRef}>
           <img className="donate-button__qrcode__image" src={QrCode} />
         </a>
       </div>
