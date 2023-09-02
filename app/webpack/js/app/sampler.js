b8d127353fe292324cdeaa1b02e06369fe55b938
Jon Rogers <jon@carbonfive.com>
2023-09-02 15:15:09 -0700

    update sampler js to be smarter about overlapping requests
    Problem
    ------
    
    If you scroll really fast, the frontend can queue up overlapping
    requests leading to both duplicate images and more load on the server.
    
    Solution
    ---------
    
    Keep track of inflight requests and don't fire new requests until any
    in flight requests are finished.

--- app/webpack/js/app/sampler.js
+++ app/webpack/js/app/sampler.js
@@ -3,10 +3,32 @@ import jQuery from "jquery";
 
 const NUM_IMAGES = 12;
 
+class RequestTracker {
+  constructor() {
+    this.requests = new Set();
+  }
+
+  static requestKey(seed, offset, count) {
+    return [seed, offset, count].join("_");
+  }
+
+  isInFlight(seed, offset, count) {
+    return this.requests.has(RequestTracker.requestKey(seed, offset, count));
+  }
+
+  start(seed, offset, count) {
+    return this.requests.add(RequestTracker.requestKey(seed, offset, count));
+  }
+
+  stop(seed, offset, count) {
+    return this.requests.delete(RequestTracker.requestKey(seed, offset, count));
+  }
+}
+
 class Sampler {
   constructor(containerSelector) {
     this.$container = jQuery(containerSelector);
-
+    this.requests = new RequestTracker();
     const $win = jQuery(window);
     $win.scroll(() => {
       if ($win.scrollTop() === jQuery(document).height() - $win.height()) {
@@ -16,17 +38,28 @@ class Sampler {
     this.fetchArtists();
   }
 
+  static requestKey(seed, offset, count) {
+    return [seed, offset, count].join("_");
+  }
+
   async fetchArtists() {
     if (!this.$container.find("#js-scroll-load-more").length) {
       return;
     }
     const seed = this.$container.data("seed");
     const offset = this.$container.data("offset") || 0;
+
+    if (this.requests.isInFlight(seed, offset, NUM_IMAGES)) {
+      return;
+    }
+
+    this.requests.start(seed, offset, NUM_IMAGES);
     const data = await post("/main/sampler", {
       seed: seed,
       offset: offset,
       number_of_images: NUM_IMAGES,
     });
+    this.requests.stop(seed, offset, NUM_IMAGES);
 
     if (data && !/^\s+$/.test(data)) {
       const $win = jQuery(window);
