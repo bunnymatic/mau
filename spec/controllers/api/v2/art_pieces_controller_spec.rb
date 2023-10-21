855a15bca557b07616a4a1dd3a5af546d1e05329
Jon Rogers <jon@carbonfive.com>
2023-10-21 10:27:41 -0700

    Add limit to art pieces controller for api calls
    Problem
    ---------
    
    when we call this from Bryant Street studios, we really don't need all
    20 art pieces.  we could use less and that'd be more efficient since we
    know some of the ActiveStorage stuff is pretty slow.
    
    Solution
    ---------
    
    Add a counter to the endpoint so we can limit the result set.
    
    Changes
    -------
    
    * new `count` param
    * add binstubs

--- spec/controllers/api/v2/art_pieces_controller_spec.rb
+++ spec/controllers/api/v2/art_pieces_controller_spec.rb
@@ -4,19 +4,53 @@ describe Api::V2::ArtPiecesController do
   describe '#index' do
     render_views
 
-    before do
-      allow(controller).to receive(:require_authorization).and_return true
+    context 'when there is art for an artist' do
+      let(:artist) { create(:artist, :active) }
+      let(:art_pieces) { create_list(:art_piece, 3, artist:) }
+
+      before do
+        allow(controller).to receive(:require_authorization).and_return true
+        art_pieces
+      end
+
+      it 'returns art pieces' do
+        get :index, params: { artist_id: artist.id, format: :json }
+
+        expect(response).to be_successful
+        json = JSON.parse(response.body)['data']
+        expect(json).to have(3).art_pieces
+      end
+
+      context 'with count parameter' do
+        it 'returns no more than count pieces' do
+          get :index, params: { count: 2, artist_id: artist.id, format: :json }
+
+          expect(response).to be_successful
+          json = JSON.parse(response.body)['data']
+          expect(json).to have(2).art_pieces
+        end
+
+        it 'returns all pieces if count is bigger than all' do
+          get :index, params: { count: 20, artist_id: artist.id, format: :json }
+
+          expect(response).to be_successful
+          json = JSON.parse(response.body)['data']
+          expect(json).to have(3).art_pieces
+        end
+      end
     end
 
-    it 'returns nothing for an unknown artist' do
-      get :index, params: { artist_id: 123, format: :json }
-      expect(response).to be_successful
-      expect(response.body).to eq '{}'
-    end
+    context 'for unknown artist' do
+      it 'returns nothing' do
+        get :index, params: { artist_id: 123, format: :json }
+        expect(response).to be_successful
+        expect(response.body).to eq '{}'
+      end
 
-    it 'includes no robot headers' do
-      get :index, params: { artist_id: 123, format: :json }
-      expect(response.headers['X-Robots-Tag']).to eq('noindex')
+      it 'includes no robot headers' do
+        get :index, params: { artist_id: 123, format: :json }
+        expect(response.headers['X-Robots-Tag']).to eq('noindex')
+      end
     end
   end
 end
