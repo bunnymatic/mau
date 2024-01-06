dafedeeba74585c24d3c6b1b5e168fb9fda88146
Jon Rogers <jon@carbonfive.com>
2024-01-05 19:37:09 -0800

    Notify BryantStreet via webhook when artist is updated
    problem
    ---------
    
    we need to notify BryantStreetStudios when an artist changes their
    profile data or their art.
    
    solution
    ---------
    
    add a webhook and ping it when artists change any data or art pieces
    change.

---
+++ app/services/bryant_street_studios_webhook.rb
@@ -0,0 +1,25 @@
+class BryantStreetStudiosWebhook
+  def artist_updated(artist_id)
+    webhook(:post, "/artist/#{artist_id}/update")
+  end
+
+  def webhook(method, path)
+    conn = Faraday.new(url(path)) do |c|
+      c.use FaradayMiddleware::FollowRedirects, limit: 3
+      c.adapter :typhoeus
+    end
+    headers = {
+      'bss-api-authorization': Conf.bryant_street_studios_api_key,
+    }
+    response = conn.send(method, url(path), headers:)
+    response.status == 200
+  rescue Errno::ECONNREFUSED, Faraday::ConnectionFailed
+    false
+  end
+
+  private
+
+  def url(path)
+    Conf.bryant_street_studios_webhook_url + path
+  end
+end
