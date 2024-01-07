132251a1b1626f6e4c920e1c182cda82eb272850
Jon Rogers <jon@carbonfive.com>
2024-01-07 11:41:15 -0800

    Update webhook to use hedaers (duh) and add tests

--- app/services/bryant_street_studios_webhook.rb
+++ app/services/bryant_street_studios_webhook.rb
@@ -1,23 +1,37 @@
 class BryantStreetStudiosWebhook
   def self.artist_updated(artist_id)
-    webhook(:post, "/artist/#{artist_id}/update")
+    post_webhook("/artist/#{artist_id}/update")
   end
 
-  def self.webhook(method, path)
-    conn = Faraday.new(url(path)) do |c|
-      c.use FaradayMiddleware::FollowRedirects, limit: 3
-      c.adapter :typhoeus
+  class << self
+    private
+
+    def post_webhook(path)
+      return false unless activated?
+
+      conn = Faraday.new(url(path)) do |c|
+        c.use FaradayMiddleware::FollowRedirects, limit: 3
+        c.adapter :typhoeus
+      end
+      headers = {
+        'bss-api-authorization' => Conf.bryant_street_studios_api_key,
+      }
+      response = conn.post(url(path), {}, headers)
+      response.status == 200
+    rescue Errno::ECONNREFUSED, Faraday::ConnectionFailed
+      false
+    rescue StandardError => e
+      Rails.logger.error('Something went wrong posting the webhook')
+      Rails.logger.error(e)
+      false
     end
-    headers = {
-      'bss-api-authorization': Conf.bryant_street_studios_api_key,
-    }
-    response = conn.send(method, url(path), headers:)
-    response.status == 200
-  rescue Errno::ECONNREFUSED, Faraday::ConnectionFailed
-    false
-  end
 
-  def self.url(path)
-    Conf.bryant_street_studios_webhook_url + path
+    def activated?
+      Conf.bryant_street_studios_webhook_url.present? && Conf.bryant_street_studios_api_key.present?
+    end
+
+    def url(path)
+      Conf.bryant_street_studios_webhook_url + path
+    end
   end
 end
