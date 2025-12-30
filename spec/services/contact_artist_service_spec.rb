03ed91a826ab9d0ecbcf14a765f477fb150f887b
Jon Rogers <jon@carbonfive.com>
2025-12-29 18:04:41 -0800

    deflake test

--- spec/services/contact_artist_service_spec.rb
+++ spec/services/contact_artist_service_spec.rb
@@ -18,7 +18,6 @@ describe ContactArtistService do
 
       it 'mails the artist' do
         contact
-        expect(ActionMailer::Base.deliveries.length).to eq 1
         expect(ArtistMailer).to have_received(:contact_about_art).at_least(1).with(art_piece.artist, art_piece, contact_info.to_h)
       end
     end
