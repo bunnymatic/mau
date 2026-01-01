182be69e9970348c93718a52e8baa00acc775674
Jon Rogers <jon@carbonfive.com>
2025-12-31 17:36:53 -0800

    update linked files for credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,15 @@ set :assets_dir, 'public/vite'
 # set :keep_releases, 5
 
 # Default value for :linked_files is []
-set :linked_files, %w[config/database.yml config/config.keys.yml config/secrets.yml config/puma.rb config/storage.yml]
+set :linked_files,
+    %w[
+      config/database.yml
+      config/credentials/production.key
+      config/credentials/acceptance.key
+      config/config.keys.yml
+      config/puma.rb
+      config/storage.yml
+    ]
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w[log tmp/pids tmp/cache tmp/sockets]
